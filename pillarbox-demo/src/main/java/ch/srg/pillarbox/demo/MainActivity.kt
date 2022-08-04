f111611d9a7b3186931591c16acb0346e55047cd
Joaquim Stähli <joaquim.staehli@gmail.com>
2022-08-04 14:06:34 +0200

    2 setup android studio project (#5)
    * Setup basic project
    
    * Configure copyright
    
    * Add first module to project

---
+++ pillarbox-demo/src/main/java/ch/srg/pillarbox/demo/MainActivity.kt
@@ -0,0 +1,21 @@
+/*
+ * Copyright (c) 2022-2022.  SRG SSR. All rights reserved.
+ * License information is available from the LICENSE file.
+ */
+package ch.srg.pillarbox.demo
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+/**
+ * Copyright (c) SRG SSR. All rights reserved.
+ * <p>
+ * License information is available from the LICENSE file.
+ */
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
