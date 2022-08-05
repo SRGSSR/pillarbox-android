0e0e940a812ca00c497dc7c1991c2145d710bc31
Joaquim Stähli <joaquim.staehli@gmail.com>
2022-08-05 12:16:46 +0200

    Install detekt

--- pillarbox-demo/src/test/java/ch/srg/pillarbox/demo/ExampleUnitTest.kt
+++ pillarbox-demo/src/test/java/ch/srg/pillarbox/demo/ExampleUnitTest.kt
@@ -4,9 +4,8 @@
  */
 package ch.srg.pillarbox.demo
 
+import org.junit.Assert.assertEquals
 import org.junit.Test
-
-import org.junit.Assert.*
 
 /**
  * Example local unit test, which will execute on the development machine (host).
