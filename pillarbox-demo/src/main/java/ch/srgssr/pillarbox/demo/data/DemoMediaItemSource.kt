7433c77a461596c981b59f4b916bdb5da2d65c4f
Joaquim Stähli <joaquim.staehli@gmail.com>
2022-08-29 18:03:43 +0200

    Update with flow api

--- pillarbox-demo/src/main/java/ch/srgssr/pillarbox/demo/data/DemoMediaItemSource.kt
+++ pillarbox-demo/src/main/java/ch/srgssr/pillarbox/demo/data/DemoMediaItemSource.kt
@@ -6,6 +6,8 @@ package ch.srgssr.pillarbox.demo.data
 
 import androidx.media3.common.MediaItem
 import ch.srgssr.pillarbox.player.data.MediaItemSource
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flowOf
 
 /**
  * A very simple [MediaItemSource] that provide [MediaItem] from [DemoItemDataSource]
@@ -13,19 +15,20 @@ import ch.srgssr.pillarbox.player.data.MediaItemSource
 class DemoMediaItemSource(demoDataSource: DemoItemDataSource) : MediaItemSource {
     private val demoItemList = demoDataSource.loadDemoItemFromAssets("streams.json")
 
-    override suspend fun loadMediaItem(mediaItem: MediaItem): MediaItem {
+    override fun loadMediaItem(mediaItem: MediaItem): Flow<MediaItem> {
         val demoItem = demoItemList.find { demoItem -> demoItem.type == ItemType.MEDIA && demoItem.id == mediaItem.mediaId }
-        // delay(5000)
         demoItem?.let {
-            return mediaItem.buildUpon()
-                .setMediaMetadata(
-                    mediaItem.mediaMetadata.buildUpon()
-                        .setTitle(it.title)
-                        .setDescription(it.description)
-                        .build()
-                )
-                .setUri(it.uri)
-                .build()
+            return flowOf(
+                mediaItem.buildUpon()
+                    .setMediaMetadata(
+                        mediaItem.mediaMetadata.buildUpon()
+                            .setTitle(it.title)
+                            .setDescription(it.description)
+                            .build()
+                    )
+                    .setUri(it.uri)
+                    .build()
+            )
         } ?: throw IllegalArgumentException("mediaId = ${mediaItem.mediaId} no found")
     }
 }
