9e5827f79da0b80c842cfec6ea626cdd0c4a6a81
Joaquim Stähli <joaquim.staehli@gmail.com>
2022-10-24 10:59:15 +0200

    Add drm test

--- pillarbox-core-business/src/test/java/ch/srg/pillarbox/core/business/ResourceSelectorTest.kt
+++ pillarbox-core-business/src/test/java/ch/srg/pillarbox/core/business/ResourceSelectorTest.kt
@@ -5,6 +5,7 @@
 package ch.srg.pillarbox.core.business
 
 import ch.srg.pillarbox.core.business.integrationlayer.data.Chapter
+import ch.srg.pillarbox.core.business.integrationlayer.data.Drm
 import ch.srg.pillarbox.core.business.integrationlayer.data.Resource
 import org.junit.Assert
 import org.junit.Test
@@ -128,6 +129,61 @@ class ResourceSelectorTest {
         Assert.assertEquals(createResource(type), result)
     }
 
+    @Test
+    fun testUnsupportedDrmOnly() {
+        val chapter = createChapter(listOf(createUnsupportedDrmResource()))
+        val result = resourceSelector.selectResourceFromChapter(chapter)
+        Assert.assertNull(result)
+    }
+
+    @Test
+    fun testUnsupportedDrm() {
+        val type = Resource.Type.HLS
+        val chapter = createChapter(listOf(
+            createUnsupportedDrmResource(),
+            createResource(type))
+        )
+        val result = resourceSelector.selectResourceFromChapter(chapter)
+        Assert.assertNotNull(result)
+        Assert.assertEquals(createResource(type), result)
+    }
+
+    @Test
+    fun testSupportedDrmOnly() {
+        val chapter = createChapter(listOf(
+            createSupportedDrmResource()
+        ))
+        val result = resourceSelector.selectResourceFromChapter(chapter)
+        Assert.assertNotNull(result)
+        Assert.assertEquals(createSupportedDrmResource(), result)
+    }
+
+    @Test
+    fun testSupportedDrm() {
+        val chapter = createChapter(listOf(
+            createResource(Resource.Type.HLS),
+            createResource(Resource.Type.DASH),
+            createSupportedDrmResource()
+        ))
+        val result = resourceSelector.selectResourceFromChapter(chapter)
+        Assert.assertNotNull(result)
+        Assert.assertEquals(createResource(Resource.Type.HLS), result)
+    }
+
+    @Test
+    fun testSupportedAndUnsupportedDrm() {
+        val chapter = createChapter(listOf(
+            createUnsupportedDrmResource(),
+            createSupportedDrmResource(),
+            createResource(Resource.Type.HLS),
+            createResource(Resource.Type.DASH),
+            createResource(Resource.Type.RTMP)
+        ))
+        val result = resourceSelector.selectResourceFromChapter(chapter)
+        Assert.assertNotNull(result)
+        Assert.assertEquals(createSupportedDrmResource(), result)
+    }
+
     companion object {
         fun createChapter(listResource: List<Resource>?): Chapter {
             return Chapter(urn = "urn", listResource = listResource, title = "title")
@@ -135,6 +191,16 @@ class ResourceSelectorTest {
 
         fun createResource(type: Resource.Type): Resource {
             return Resource(url = "", type = type)
+        }
+
+        fun createUnsupportedDrmResource(): Resource {
+            return Resource("", Resource.Type.HLS, drmList = listOf(Drm(Drm.Type.FAIRPLAY, "")))
+        }
+
+        fun createSupportedDrmResource(): Resource {
+            return Resource("", Resource.Type.DASH, drmList = listOf(
+                Drm(Drm.Type.WIDEVINE, "https://widevine.license.co"),
+                Drm(Drm.Type.PLAYREADY, "https://playready.license.co")))
         }
     }
 }
