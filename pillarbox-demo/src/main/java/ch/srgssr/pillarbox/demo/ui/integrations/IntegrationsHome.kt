15a21c37e0711f525eddbc8605d0573371f37eb0
Joaquim Stähli <joaquim.staehli@gmail.com>
2022-11-24 09:33:43 +0100

    Move story to old playlist home view

--- pillarbox-demo/src/main/java/ch/srgssr/pillarbox/demo/ui/integrations/IntegrationsHome.kt
+++ pillarbox-demo/src/main/java/ch/srgssr/pillarbox/demo/ui/integrations/IntegrationsHome.kt
@@ -2,7 +2,7 @@
  * Copyright (c) 2022. SRG SSR. All rights reserved.
  * License information is available from the LICENSE file.
  */
-package ch.srgssr.pillarbox.demo.ui.playlists
+package ch.srgssr.pillarbox.demo.ui.integrations
 
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
@@ -15,49 +15,76 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import ch.srgssr.pillarbox.demo.data.Playlist
+import androidx.navigation.NavController
+import ch.srgssr.pillarbox.demo.ui.NavigationRoutes
 import ch.srgssr.pillarbox.demo.ui.player.SimplePlayerActivity
 import ch.srgssr.pillarbox.demo.ui.theme.PillarboxTheme
 
 /**
- * Playlists home view that display list of clickable playlist
+ * Integrations home that displays integrations sample.
+ * For exemple, playlists, story, ...
  *
+ * @param navController
  * @param playlistsViewModel
  */
 @Composable
-fun PlaylistsHome(playlistsViewModel: PlaylistsViewModel) {
+fun IntegrationsHome(navController: NavController, playlistsViewModel: PlaylistsViewModel) {
     val context = LocalContext.current
     val listItems = playlistsViewModel.listPlaylist.collectAsState()
     Column {
+        HeaderView(header = "Playlists")
+        Divider()
         for (playlist in listItems.value) {
-            PlaylistItemView(playlist = playlist) {
+            ItemView(title = playlist.title) {
                 SimplePlayerActivity.startActivity(context, playlist)
             }
             Divider()
+        }
+        ItemView(title = "Story") {
+            navController.navigate(NavigationRoutes.story)
         }
     }
 }
 
 @Composable
-private fun PlaylistItemView(playlist: Playlist, onItemClicked: (Playlist) -> Unit) {
+private fun ItemView(title: String, onClick: () -> Unit) {
     Text(
         modifier = Modifier
             .fillMaxWidth()
-            .clickable { onItemClicked(playlist) }
+            .clickable { onClick() }
             .padding(8.dp),
-        text = playlist.title,
+        text = title,
         style = MaterialTheme.typography.body1
+    )
+}
+
+@Composable
+private fun HeaderView(header: String) {
+    Text(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(8.dp),
+        text = header,
+        fontWeight = FontWeight.Bold,
+        style = MaterialTheme.typography.h4
     )
 }
 
 @Preview(showBackground = true)
 @Composable
-private fun PreviewPlaylistItem() {
+private fun PreviewItems() {
     PillarboxTheme {
-        PlaylistItemView(playlist = Playlist("Playlist Title", emptyList())) {
-            // Nothing
+        Column() {
+            HeaderView(header = "Header 1")
+            ItemView("Item 1") {}
+            Divider()
+            ItemView("Item 2") {}
+            Divider()
+            HeaderView(header = "header 2")
+            ItemView("Item 3") {}
         }
     }
 }
