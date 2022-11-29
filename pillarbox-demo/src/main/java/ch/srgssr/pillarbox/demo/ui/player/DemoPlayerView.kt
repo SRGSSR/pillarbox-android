d3438f6c354965b4dfdf764c29c25f3266f55b8f
Joaquim Stähli <joaquim.staehli@gmail.com>
2022-11-29 15:04:11 +0100

    Redo DemoPlayer controls view

--- pillarbox-demo/src/main/java/ch/srgssr/pillarbox/demo/ui/player/DemoPlayerView.kt
+++ pillarbox-demo/src/main/java/ch/srgssr/pillarbox/demo/ui/player/DemoPlayerView.kt
@@ -10,18 +10,24 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentHeight
-import androidx.compose.material.Button
+import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.material.Icon
 import androidx.compose.material.IconButton
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Switch
 import androidx.compose.material.Text
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.PictureInPicture
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.collectAsState
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.viewinterop.AndroidView
 import androidx.media3.common.Player
 import ch.srgssr.pillarbox.demo.ui.SRGErrorMessageProvider
@@ -39,7 +45,6 @@ fun DemoPlayerView(
     playerViewModel: SimplePlayerViewModel,
     pipClick: () -> Unit = {}
 ) {
-    val pauseOnBackground = playerViewModel.pauseOnBackground.collectAsState()
     val hideUi = playerViewModel.pictureInPictureEnabled.collectAsState()
     Column(modifier = Modifier.fillMaxSize()) {
         PlayerView(
@@ -50,14 +55,37 @@ fun DemoPlayerView(
                 .wrapContentHeight()
         )
         if (!hideUi.value) {
-            Row(modifier = Modifier.fillMaxWidth()) {
-                Button(onClick = { playerViewModel.togglePauseOnBackground() }) {
-                    Text(text = if (pauseOnBackground.value) "Pause on background" else "Continue on background")
-                }
-                IconButton(onClick = pipClick) {
-                    Icon(imageVector = Icons.Default.PictureInPicture, contentDescription = "Go in picture in picture")
-                }
+            DemoControlView(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(8.dp),
+                playerViewModel = playerViewModel, pipClick = pipClick
+            )
+        }
+    }
+}
+
+@Composable
+private fun DemoControlView(
+    modifier: Modifier = Modifier,
+    playerViewModel: SimplePlayerViewModel,
+    pipClick: () -> Unit = {}
+) {
+    val pauseOnBackground = playerViewModel.pauseOnBackground.collectAsState()
+    Column(modifier = modifier) {
+        Row {
+            IconButton(onClick = pipClick) {
+                Icon(imageVector = Icons.Default.PictureInPicture, contentDescription = "Go in picture in picture")
             }
+        }
+        Row(verticalAlignment = Alignment.CenterVertically) {
+            Text(
+                modifier = Modifier.wrapContentWidth(),
+                text = "Background playback",
+                style = MaterialTheme.typography.caption,
+                fontWeight = FontWeight.Bold
+            )
+            Switch(checked = !pauseOnBackground.value, onCheckedChange = { playerViewModel.togglePauseOnBackground() })
         }
     }
 }
