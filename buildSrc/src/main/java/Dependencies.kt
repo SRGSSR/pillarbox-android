645c57681b774b46e695a1c04ae9e680deec689e
Joaquim Stähli <joaquim.staehli@gmail.com>
2022-12-14 16:17:55 +0100

    update fragment version

--- buildSrc/src/main/java/Dependencies.kt
+++ buildSrc/src/main/java/Dependencies.kt
@@ -3,6 +3,7 @@
  * License information is available from the LICENSE file.
  */
 
+// https://developer.android.com/jetpack/androidx/explorer
 object Version {
     const val core = "1.8.0"
     const val coroutines = "1.6.4"
@@ -18,7 +19,7 @@ object Version {
     const val androidJunit = "1.1.3"
     const val espresso = "3.4.0"
     const val lifecycle = "2.5.1"
-    const val fragment = "1.5.4"
+    const val fragment = "1.5.5"
     const val navigation = "2.5.3"
     const val activity = "1.6.1"
     const val composeCompiler = "1.3.2"
