7fdab849a7a7d190fd2f32695c900398a219a52e
Joaquim Stähli <joaquim.staehli@gmail.com>
2023-01-19 09:59:48 +0100

    Update doc

--- pillarbox-ui/src/main/java/ch/srgssr/pillarbox/ui/ScaleMode.kt
+++ pillarbox-ui/src/main/java/ch/srgssr/pillarbox/ui/ScaleMode.kt
@@ -6,9 +6,25 @@ package ch.srgssr.pillarbox.ui
 
 /**
  * Scale mode
- *
- * @constructor Create empty Scale mode
  */
 enum class ScaleMode {
-    Fit, Fill, Crop, Zoom
+    /**
+     * Fit player content to the parent container and keep aspect ratio.
+     */
+    Fit,
+
+    /**
+     * Fill player content to the parent container.
+     */
+    Fill,
+
+    /**
+     * Crop player content inside the parent container and keep aspect ratio. Content outside the parent container will be clipped.
+     */
+    Crop,
+
+    /**
+     * Zoom, like [Crop] but doesn't clip content to the parent container. Useful for fullscreen mode.
+     */
+    Zoom
 }
