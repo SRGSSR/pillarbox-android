47eede3b76ad3563c3fb078a40896f60cdf61dc6
Joaquim Stähli <joaquim.staehli@gmail.com>
2023-02-27 08:07:39 +0100

    Add Commands extension and update doc

--- pillarbox-player/src/main/java/ch/srgssr/pillarbox/player/PlayerCommandsExtentions.kt
+++ pillarbox-player/src/main/java/ch/srgssr/pillarbox/player/PlayerCommandsExtentions.kt
@@ -2,48 +2,71 @@
  * Copyright (c) 2023. SRG SSR. All rights reserved.
  * License information is available from the LICENSE file.
  */
+@file:Suppress("unused")
+
 package ch.srgssr.pillarbox.player
 
 import androidx.media3.common.Player
 
 /**
- * Can seek to next
+ * Can seek to a later position in the current or next MediaItem.
  */
 fun Player.Commands.canSeekToNext(): Boolean {
     return contains(Player.COMMAND_SEEK_TO_NEXT)
 }
 
 /**
- * Can seek to previous
+ * Can seek to an earlier position in the current or previous MediaItem.
  */
 fun Player.Commands.canSeekToPrevious(): Boolean {
     return contains(Player.COMMAND_SEEK_TO_PREVIOUS)
 }
 
 /**
- * Can seek forward
+ * Can seek back by a fixed increment into the current MediaItem.
  */
 fun Player.Commands.canSeekForward(): Boolean {
     return contains(Player.COMMAND_SEEK_FORWARD)
 }
 
 /**
- * Can seek back
+ * Can seek back by a fixed increment into the current MediaItem.
  */
 fun Player.Commands.canSeekBack(): Boolean {
     return contains(Player.COMMAND_SEEK_BACK)
 }
 
 /**
- * Can seek in current MediaItem
+ * Can seek anywhere into the current MediaItem.
  */
 fun Player.Commands.canSeek(): Boolean {
     return contains(Player.COMMAND_SEEK_IN_CURRENT_MEDIA_ITEM)
 }
 
 /**
- * Can play and pause
+ * Can start, pause or resume playback.
  */
 fun Player.Commands.canPlayPause(): Boolean {
     return contains(Player.COMMAND_PLAY_PAUSE)
+}
+
+/**
+ * Can get details of the current track selection.
+ */
+fun Player.Commands.canGetTracks(): Boolean {
+    return contains(Player.COMMAND_GET_TRACKS)
+}
+
+/**
+ * set the player's track selection parameters.
+ */
+fun Player.Commands.canSetTrackSelectionParameters(): Boolean {
+    return contains(Player.COMMAND_SET_TRACK_SELECTION_PARAMETERS)
+}
+
+/**
+ * Can set the playback speed and pitch.
+ */
+fun Player.Commands.canSpeedAndPitch(): Boolean {
+    return contains(Player.COMMAND_SET_SPEED_AND_PITCH)
 }
