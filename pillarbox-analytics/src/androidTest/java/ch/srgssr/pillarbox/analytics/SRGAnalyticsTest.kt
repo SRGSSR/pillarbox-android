f3f0af56b493f5eaa85fb31d789552e6f4c3b7be
Joaquim Stähli <joaquim.staehli@gmail.com>
2023-04-03 13:29:24 +0200

    Make SRGAnalytics singleton

---
+++ pillarbox-analytics/src/androidTest/java/ch/srgssr/pillarbox/analytics/SRGAnalyticsTest.kt
@@ -0,0 +1,33 @@
+/*
+ * Copyright (c) 2023. SRG SSR. All rights reserved.
+ * License information is available from the LICENSE file.
+ */
+package ch.srgssr.pillarbox.analytics
+
+import androidx.test.platform.app.InstrumentationRegistry
+import ch.srgssr.pillarbox.analytics.commandersact.CommandersAct
+import org.junit.Assert
+import org.junit.Test
+
+class SRGAnalyticsTest {
+
+    private val config = SRGAnalytics.Config(
+        analyticsConfig = AnalyticsConfig(distributor = AnalyticsConfig.BuDistributor.SRG, "pillarbox-test-android"),
+        commandersAct = CommandersAct.Config.SRG_DEBUG
+    )
+
+    @Test
+    fun testInitTwice() {
+        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
+        val analytics = SRGAnalytics.init(appContext = appContext, config = config)
+        Assert.assertEquals(analytics, SRGAnalytics.init(appContext, config))
+    }
+
+    @Test(expected = IllegalArgumentException::class)
+    fun testInitTwiceDifferentConfig() {
+        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
+        val analytics = SRGAnalytics.init(appContext = appContext, config = config)
+        val config2 = config.copy(analyticsConfig = AnalyticsConfig(distributor = AnalyticsConfig.BuDistributor.RSI, "pillarbox-test-fail"))
+        Assert.assertEquals(analytics, SRGAnalytics.init(appContext, config2))
+    }
+}
--- pillarbox-analytics/src/main/java/ch/srgssr/pillarbox/analytics/SRGAnalytics.kt
+++ pillarbox-analytics/src/main/java/ch/srgssr/pillarbox/analytics/SRGAnalytics.kt
@@ -14,32 +14,56 @@ import ch.srgssr.pillarbox.analytics.comscore.ComScore
  * @param appContext Application context.
  * @param config Global analytics config.
  */
-class SRGAnalytics(appContext: Context, config: Config) : AnalyticsDelegate {
+object SRGAnalytics : AnalyticsDelegate {
+    private var config: Config? = null
+    private var _commandersAct: CommandersAct? = null
+    private var _comScore: ComScore? = null
 
     /**
      * TagCommander analytics
      */
-    val commandersAct by lazy {
-        CommandersAct(
-            appContext = appContext,
-            config = config.analyticsConfig,
-            commandersActConfig = config.commandersAct
-        )
-    }
+    val commandersAct: CommandersAct
+        get() = _commandersAct!!
 
     /**
      * ComScore analytics
      */
-    val comScore by lazy { ComScore.init(config.analyticsConfig, config.comScore, appContext) }
+    val comScore: ComScore
+        get() = _comScore!!
+
+    /**
+     * Init SRGAnalytics
+     *
+     * @param appContext Application context
+     * @param config SRGAnalytics configuration
+     */
+    fun init(appContext: Context, config: Config): SRGAnalytics {
+        if (this.config != null) {
+            require(this.config == config) { "Already init with another config" }
+            return this
+        }
+        return synchronized(this) {
+            this.config = config
+            _commandersAct = CommandersAct(config = config.analyticsConfig, commandersActConfig = config.commandersAct, appContext)
+            _comScore = ComScore.init(config = config.analyticsConfig, comScoreConfig = config.comScore, appContext)
+            this
+        }
+    }
 
     override fun sendPageViewEvent(pageEvent: PageEvent) {
+        checkInitialized()
         commandersAct.sendPageViewEvent(pageEvent)
         comScore.sendPageViewEvent(pageEvent)
     }
 
     override fun sendEvent(event: Event) {
+        checkInitialized()
         commandersAct.sendEvent(event)
         // Business decision to not send those event to comScore.
+    }
+
+    private fun checkInitialized() {
+        requireNotNull(config) { "SRGAnalytics init has to be called before!" }
     }
 
     /**
