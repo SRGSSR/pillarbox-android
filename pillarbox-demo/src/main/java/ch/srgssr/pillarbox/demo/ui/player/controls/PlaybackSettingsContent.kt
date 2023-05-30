58cc04d4945f1f3eaa23a402e2facb28e2c865c4
Joaquim Stähli <joaquim.staehli@gmail.com>
2023-05-30 15:48:15 +0200

    Deselect playback setting when clicked

--- pillarbox-demo/src/main/java/ch/srgssr/pillarbox/demo/ui/player/controls/PlaybackSettingsContent.kt
+++ pillarbox-demo/src/main/java/ch/srgssr/pillarbox/demo/ui/player/controls/PlaybackSettingsContent.kt
@@ -29,8 +29,6 @@ import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.semantics.Role
 import androidx.media3.common.Player
 import ch.srgssr.pillarbox.player.PlayerState
-import ch.srgssr.pillarbox.player.canSpeedAndPitch
-import ch.srgssr.pillarbox.ui.availableCommands
 import ch.srgssr.pillarbox.ui.rememberPlayerState
 
 private val speeds = mapOf(
@@ -60,24 +58,24 @@ fun ColumnScope.PlaybackSettingsContent(
         onDismiss
     }
     val currentPlaybackSpeed = playerState.playbackSpeed.collectAsState()
-    val commands = playerState.availableCommands()
     val currentSpeedLabel = remember {
         derivedStateOf {
             speeds.entries.first { it.value == currentPlaybackSpeed.value }.key
         }
     }
-    var playbackSettingsSelected by remember(currentPlaybackSpeed.value) {
+    var playbackSettingsSelected by remember {
         mutableStateOf(false)
     }
 
     if (playbackSettingsSelected) {
         for (speed in speeds) {
-            val enabled = commands.canSpeedAndPitch() && currentSpeedLabel.value == speed.key
+            val enabled = currentSpeedLabel.value == speed.key
             SettingsOptionItem(
                 title = speed.key,
                 enabled = enabled,
                 modifier = Modifier.toggleable(enabled) {
                     player.setPlaybackSpeed(speed.value)
+                    playbackSettingsSelected = false
                     onDismissState.invoke()
                 }
             )
