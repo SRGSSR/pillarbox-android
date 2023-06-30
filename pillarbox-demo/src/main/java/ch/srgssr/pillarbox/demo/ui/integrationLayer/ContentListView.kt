65a330c3d68cc4240891ed969012d65ea440d5af
Joaquim Stähli <joaquim.staehli@gmail.com>
2023-06-30 15:09:43 +0200

    Pretty content view

--- pillarbox-demo/src/main/java/ch/srgssr/pillarbox/demo/ui/integrationLayer/ContentListView.kt
+++ pillarbox-demo/src/main/java/ch/srgssr/pillarbox/demo/ui/integrationLayer/ContentListView.kt
@@ -8,13 +8,16 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
+import androidx.compose.material.minimumInteractiveComponentSize
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.paging.compose.LazyPagingItems
 import androidx.paging.compose.collectAsLazyPagingItems
 import androidx.paging.compose.itemKey
@@ -35,14 +38,16 @@ fun ContentListView(
     contentClick: (Content) -> Unit = {}
 ) {
     val lazyPagingItems: LazyPagingItems<Content> = contentListViewModel.data.collectAsLazyPagingItems()
-    LazyColumn(modifier = modifier) {
+    LazyColumn(modifier = modifier.padding(horizontal = 16.dp)) {
         items(count = lazyPagingItems.itemCount, key = lazyPagingItems.itemKey()) { index ->
             val item = lazyPagingItems[index]
             item?.let {
                 ContentView(
                     content = it,
                     Modifier
+                        .padding(bottom = 2.dp)
                         .fillMaxWidth()
+                        .minimumInteractiveComponentSize()
                         .clickable { contentClick(it) }
                 )
             }
