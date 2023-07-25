4e6b10cea7c642683dd1c92e38a8947d32c9308c
Joaquim Stähli <joaquim.staehli@gmail.com>
2023-07-25 14:54:47 +0200

    Add additional debug infos

--- buildSrc/src/main/java/VersionConfig.kt
+++ buildSrc/src/main/java/VersionConfig.kt
@@ -34,7 +34,7 @@ object VersionConfig {
      */
     fun semanticVersion(): String? {
         System.out.println("version = $ENV_VERSION_NAME")
-        System.out.println("fromCI = $isCI")
+        System.out.println("fromCI = ${System.getenv("CI")} = $isCI")
         return ENV_VERSION_NAME?.let { versionOnlyRegex.find(it)?.value }
     }
 
