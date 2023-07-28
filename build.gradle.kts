aff67fd42164e0466f66d26277d2a85decd9412d
Joaquim Stähli <joaquim.staehli@gmail.com>
2023-07-28 09:13:28 +0200

    Update dependencies (#192)

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,10 +5,10 @@
 import io.gitlab.arturbosch.detekt.Detekt
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    id("com.android.application") version "8.0.1" apply false
-    id("com.android.library") version "8.0.1" apply false
-    id("org.jetbrains.kotlin.kapt") version "1.8.20" apply false
-    id("org.jetbrains.kotlin.android") version "1.8.20" apply false
+    id("com.android.application") version "8.1.0" apply false
+    id("com.android.library") version "8.1.0" apply false
+    id("org.jetbrains.kotlin.kapt") version "1.9.0" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.0" apply false
     // https://github.com/detekt/detekt
     id("io.gitlab.arturbosch.detekt").version(Version.detetk)
 }
