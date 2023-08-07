435dc03aa4f62f362bb1b567d6793cf3e8d9552f
Joaquim Stähli <joaquim.staehli@gmail.com>
2023-08-07 16:02:21 +0200

    Display special icon for hard hearing text track

--- pillarbox-demo/src/main/java/ch/srgssr/pillarbox/demo/ui/player/settings/TrackSelectionSettings.kt
+++ pillarbox-demo/src/main/java/ch/srgssr/pillarbox/demo/ui/player/settings/TrackSelectionSettings.kt
@@ -4,19 +4,29 @@
  */
 package ch.srgssr.pillarbox.demo.ui.player.settings
 
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.Divider
+import androidx.compose.material.Icon
 import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.HearingDisabled
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.media3.common.C
 import androidx.media3.common.Format
 import androidx.media3.common.TrackGroup
 import androidx.media3.common.TrackSelectionParameters
 import androidx.media3.common.Tracks
 import ch.srgssr.pillarbox.demo.ui.theme.PillarboxTheme
+import ch.srgssr.pillarbox.player.extension.hasRole
+import ch.srgssr.pillarbox.player.extension.roleString
 
 /**
  * Track selection settings
@@ -69,7 +79,15 @@ fun TrackSelectionSettings(
                         }
 
                         else -> {
-                            Text(text = "${format.label} / ${format.language}")
+                            if (format.hasRole(C.ROLE_FLAG_DESCRIBES_MUSIC_AND_SOUND.or(C.ROLE_FLAG_TRANSCRIBES_DIALOG))) {
+                                Row(verticalAlignment = Alignment.CenterVertically) {
+                                    Text(text = "${format.label} / ${format.language}")
+                                    Spacer(modifier = Modifier.width(12.dp))
+                                    Icon(imageVector = Icons.Default.HearingDisabled, contentDescription = "Hearing disabled")
+                                }
+                            } else {
+                                Text(text = "${format.label} / ${format.language} ${format.roleString()}")
+                            }
                         }
                     }
                 }
