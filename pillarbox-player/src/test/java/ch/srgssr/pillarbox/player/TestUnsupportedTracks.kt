f02c8fcefcda2525fcebaf7c0e86835025e61d0f
Joaquim Stähli <joaquim.staehli@gmail.com>
2023-08-17 09:02:03 +0200

    Add more unit tests.

--- pillarbox-player/src/test/java/ch/srgssr/pillarbox/player/TestUnsupportedTracks.kt
+++ pillarbox-player/src/test/java/ch/srgssr/pillarbox/player/TestUnsupportedTracks.kt
@@ -5,6 +5,7 @@
 package ch.srgssr.pillarbox.player
 
 import androidx.media3.common.C
+import androidx.media3.common.C.FormatSupport
 import androidx.media3.common.Format
 import androidx.media3.common.MimeTypes
 import androidx.media3.common.TrackGroup
@@ -16,40 +17,107 @@ import org.junit.Test
 class TestUnsupportedTracks {
 
     @Test
-    fun test1() {
+    fun testOneSupportedTrack() {
+        val format = createSampleFormat("Unsupported")
+        val listFormat = listOf(format)
+        val trackGroup = TrackGroup(*listFormat.toTypedArray())
+        val selected = BooleanArray(listFormat.size)
+        val trackSupport = createAllSupportArray(listFormat.size, C.FORMAT_HANDLED)
+        val tracks = Tracks(listOf(Tracks.Group(trackGroup, false, trackSupport, selected)))
+        val expectedSize = 1
+        val expectedTracksGroups = listOf(
+            Tracks.Group(
+                TrackGroup(*listOf(format).toTypedArray()),
+                false,
+                createAllSupportArray(expectedSize, C.FORMAT_HANDLED),
+                BooleanArray(expectedSize)
+            )
+        )
+        Assert.assertEquals(expectedTracksGroups, tracks.groups.mapNotNull { it.filterUnsupported() })
+    }
+
+    @Test
+    fun testOnlySupportedTracks() {
+        val format1 = createSampleFormat("F1")
+        val format2 = createSampleFormat("F2")
+        val listFormat = listOf(format1, format2)
+        val trackGroup = TrackGroup(*listFormat.toTypedArray())
+        val selected = BooleanArray(listFormat.size)
+        val trackSupport = createAllSupportArray(listFormat.size, C.FORMAT_HANDLED)
+        val tracks = Tracks(listOf(Tracks.Group(trackGroup, false, trackSupport, selected)))
+        val expectedSize = 2
+        val expectedTracksGroups = listOf(
+            Tracks.Group(
+                TrackGroup(*listOf(format1, format2).toTypedArray()),
+                false,
+                createAllSupportArray(expectedSize, C.FORMAT_HANDLED),
+                BooleanArray(expectedSize)
+            )
+        )
+        Assert.assertEquals(expectedTracksGroups, tracks.groups.mapNotNull { it.filterUnsupported() })
+    }
+
+    @Test
+    fun testMultiTrackOneUnsupported() {
         val format1 = createSampleFormat("F1")
         val format2 = createSampleFormat("F2")
         val formatUnsupportedTrack = createSampleFormat("Unsupported")
         val listFormat = listOf(format1, format2, formatUnsupportedTrack)
         val trackGroup = TrackGroup(*listFormat.toTypedArray())
         val selected = BooleanArray(listFormat.size)
-        val trackSupport = IntArray(listFormat.size)
-        trackSupport[0] = C.FORMAT_HANDLED
-        trackSupport[1] = C.FORMAT_HANDLED
+        val trackSupport = createAllSupportArray(listFormat.size, C.FORMAT_HANDLED)
         trackSupport[2] = C.FORMAT_UNSUPPORTED_SUBTYPE
         val tracks = Tracks(listOf(Tracks.Group(trackGroup, false, trackSupport, selected)))
-
+        val expectedSize = 2
         val expectedTracksGroups = listOf(
             Tracks.Group(
-                TrackGroup(*listOf(format1, format2).toTypedArray()), false, IntArray(2){
-                          C.FORMAT_HANDLED
-                }, BooleanArray
-                    (2)
+                TrackGroup(*listOf(format1, format2).toTypedArray()),
+                false,
+                createAllSupportArray(expectedSize, C.FORMAT_HANDLED),
+                BooleanArray(expectedSize)
             )
         )
         Assert.assertEquals(expectedTracksGroups, tracks.groups.mapNotNull { it.filterUnsupported() })
     }
 
+    @Test
+    fun testMultipleUnsupportedTracks() {
+        val format1 = createSampleFormat("F1")
+        val format2 = createSampleFormat("F2")
+        val formatUnsupportedTrack = createSampleFormat("Unsupported")
+        val listFormat = listOf(format1, format2, formatUnsupportedTrack)
+        val trackGroup = TrackGroup(*listFormat.toTypedArray())
+        val selected = BooleanArray(listFormat.size)
+        val trackSupport = createAllSupportArray(listFormat.size, C.FORMAT_UNSUPPORTED_SUBTYPE)
+        val tracks = Tracks(listOf(Tracks.Group(trackGroup, false, trackSupport, selected)))
+        Assert.assertTrue(tracks.groups.mapNotNull { it.filterUnsupported() }.isEmpty())
+    }
+
+    @Test
+    fun testOneUnsupportedTrack() {
+        val formatUnsupportedTrack = createSampleFormat("Unsupported")
+        val listFormat = listOf(formatUnsupportedTrack)
+        val trackGroup = TrackGroup(*listFormat.toTypedArray())
+        val selected = BooleanArray(listFormat.size)
+        val trackSupport = createAllSupportArray(listFormat.size, C.FORMAT_UNSUPPORTED_SUBTYPE)
+        val tracks = Tracks(listOf(Tracks.Group(trackGroup, false, trackSupport, selected)))
+        Assert.assertTrue(tracks.groups.mapNotNull { it.filterUnsupported() }.isEmpty())
+    }
+
     companion object {
-        private fun createSampleFormat(
-            label: String
-        ):
-            Format {
+
+        private fun createSampleFormat(label: String): Format {
             return Format.Builder()
                 .setId("id:${label}")
                 .setLabel(label)
                 .setContainerMimeType(MimeTypes.AUDIO_MP4)
                 .build()
+        }
+
+        private fun createAllSupportArray(size: Int, support: @FormatSupport Int): IntArray {
+            return IntArray(size) {
+                support
+            }
         }
     }
 
