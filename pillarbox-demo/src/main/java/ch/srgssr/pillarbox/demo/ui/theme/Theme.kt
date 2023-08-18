86e2b36103a326428fa4eddd61dc4a489891cc6b
Joaquim Stähli <joaquim.staehli@gmail.com>
2023-08-18 17:29:02 +0200

    Use default material 3 theme colors.

--- pillarbox-demo/src/main/java/ch/srgssr/pillarbox/demo/ui/theme/Theme.kt
+++ pillarbox-demo/src/main/java/ch/srgssr/pillarbox/demo/ui/theme/Theme.kt
@@ -5,68 +5,39 @@
 package ch.srgssr.pillarbox.demo.ui.theme
 
 import android.app.Activity
-import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.darkColorScheme
-import androidx.compose.material3.dynamicDarkColorScheme
-import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.SideEffect
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.toArgb
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalView
 import androidx.core.view.WindowCompat
 
 // https://m3.material.io/styles/color/the-color-system/color-roles
-private val DarkColorScheme = darkColorScheme(
-    primary = Gray16,
-    secondary = RedSrgDark,
-    tertiary = Color.Black
-)
+private val DarkColorScheme = darkColorScheme()
 
-private val LightColorScheme = lightColorScheme(
-    primary = Gray16,
-    secondary = RedSrg,
-    tertiary = Color.Black
-
-    /* Other default colors to override
-    background = Color(0xFFFFFBFE),
-    surface = Color(0xFFFFFBFE),
-    onPrimary = Color.White,
-    onSecondary = Color.White,
-    onTertiary = Color.White,
-    onBackground = Color(0xFF1C1B1F),
-    onSurface = Color(0xFF1C1B1F),
-    */
-)
+private val LightColorScheme = lightColorScheme()
 
 /**
  * Pillarbox theme
  *
  * @param darkTheme
- * @param dynamicColor
  * @param content
  * @receiver
  */
 @Composable
 fun PillarboxTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
-    // Dynamic color is available on Android 12+
-    dynamicColor: Boolean = true,
     content: @Composable () -> Unit
 ) {
-    val colorScheme = when {
-        dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
-            val context = LocalContext.current
-            if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
-        }
-
-        darkTheme -> DarkColorScheme
-        else -> LightColorScheme
+    val colorScheme = if (darkTheme) {
+        DarkColorScheme
+    } else {
+        LightColorScheme
     }
+
     val view = LocalView.current
     if (!view.isInEditMode) {
         SideEffect {
