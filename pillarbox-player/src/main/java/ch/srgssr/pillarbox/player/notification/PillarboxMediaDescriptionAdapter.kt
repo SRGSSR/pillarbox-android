ce1498c50dcb9f706dd1d1f68968d7ad17e1c4a3
Joaquim Stähli <joaquim.staehli@gmail.com>
2023-08-25 09:33:32 +0200

    Remove unecesary glide dependency

--- pillarbox-player/src/main/java/ch/srgssr/pillarbox/player/notification/PillarboxMediaDescriptionAdapter.kt
+++ pillarbox-player/src/main/java/ch/srgssr/pillarbox/player/notification/PillarboxMediaDescriptionAdapter.kt
@@ -8,14 +8,15 @@ import android.app.PendingIntent
 import android.content.Context
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
-import android.graphics.drawable.Drawable
 import android.text.TextUtils
 import androidx.media3.common.Player
 import androidx.media3.ui.PlayerNotificationManager
 import androidx.media3.ui.PlayerNotificationManager.MediaDescriptionAdapter
-import com.bumptech.glide.Glide
-import com.bumptech.glide.request.target.CustomTarget
-import com.bumptech.glide.request.transition.Transition
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.MainScope
+import kotlinx.coroutines.launch
+import java.net.URL
 
 /**
  * Pillarbox media description adapter that handle image loading with Glide.
@@ -29,6 +30,7 @@ class PillarboxMediaDescriptionAdapter(private val context: Context, private val
             .compat_notification_large_icon_max_width
     )
     private val imageMaxHeight: Int = context.resources.getDimensionPixelSize(androidx.media3.ui.R.dimen.compat_notification_large_icon_max_height)
+    private var job: Job? = null
 
     override fun getCurrentContentTitle(player: Player): CharSequence {
         val displayTitle = player.mediaMetadata.displayTitle
@@ -52,23 +54,22 @@ class PillarboxMediaDescriptionAdapter(private val context: Context, private val
     }
 
     override fun getCurrentLargeIcon(player: Player, callback: PlayerNotificationManager.BitmapCallback): Bitmap? {
+        job?.cancel()
+        job = null
         return if (player.mediaMetadata.artworkData != null) {
             val data = player.mediaMetadata.artworkData!!
             BitmapFactory.decodeByteArray(data, /* offset= */0, data.size)
         } else if (player.mediaMetadata.artworkUri != null) {
             val imageUri = player.mediaMetadata.artworkUri!!
-            Glide.with(context).asBitmap()
-                .load(imageUri)
-                .into(object : CustomTarget<Bitmap>(imageMaxWidth, imageMaxHeight) {
-                    override fun onResourceReady(resource: Bitmap, transition: Transition<in Bitmap>?) {
-                        callback.onBitmap(resource)
-                    }
-
-                    override fun onLoadCleared(placeholder: Drawable?) {
-                        // Do nothing
-                    }
-                })
-            // TODO return Bitmap placeholder while loading
+            val imageUrl = URL(imageUri.toString())
+            val opts = BitmapFactory.Options().apply {
+                outWidth = imageMaxWidth
+                outHeight = imageMaxHeight
+            }
+            job = MainScope().launch(Dispatchers.IO) {
+                val bitmap = BitmapFactory.decodeStream(imageUrl.openStream(), null, opts)
+                bitmap?.let { callback.onBitmap(bitmap) }
+            }
             null
         } else {
             null
