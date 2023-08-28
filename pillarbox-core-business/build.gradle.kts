8795ea58e353a20aca6a1fdb4f22331cf5e3cb1a
Joaquim Stähli <joaquim.staehli@gmail.com>
2023-08-28 13:45:31 +0200

    wip

--- pillarbox-core-business/build.gradle.kts
+++ pillarbox-core-business/build.gradle.kts
@@ -57,6 +57,13 @@ dependencies {
 
     implementation(libs.kotlinx.coroutines.android)
     implementation(libs.androidx.ktx)
+    implementation(libs.ktor.client.core)
+    implementation(libs.ktor.client.okhttp)
+    implementation(libs.ktor.client.serialization)
+    implementation(libs.ktor.serialization.kotlinx.json)
+    implementation(libs.ktor.client.content.negotiation)
+
+    implementation(libs.ktor.client.logging)
     api(libs.retrofit)
     implementation(libs.kotlinx.serialization.json)
     implementation(libs.retrofit2.kotlinx.serialization.converter)
