2c7a58ec82d7938df02881ab946d81a3d7ce7665
Joaquim Stähli <joaquim.staehli@gmail.com>
2023-09-14 14:10:31 +0200

    Convenient method to build MediaItem from urn.

---
+++ pillarbox-core-business/src/main/java/ch/srgssr/pillarbox/core/business/MediaItemUrn.kt
@@ -0,0 +1,36 @@
+/*
+ * Copyright (c) 2023. SRG SSR. All rights reserved.
+ * License information is available from the LICENSE file.
+ */
+package ch.srgssr.pillarbox.core.business
+
+import android.net.Uri
+import androidx.media3.common.MediaItem
+import androidx.media3.common.MediaMetadata
+
+/**
+ * Create [MediaItem] for Pillarbox from a urn.
+ */
+object MediaItemUrn {
+    /**
+     * Invoke
+     *
+     * @param urn The media urn to play.
+     * @param title The optional title to display..
+     * @param subtitle The optional subtitle to display.
+     * @param artworkUri The artworkUri image uri.
+     * @return MediaItem.
+     */
+    operator fun invoke(urn: String, title: String? = null, subtitle: String? = null, artworkUri: Uri? = null): MediaItem {
+        return MediaItem.Builder()
+            .setMediaId(urn)
+            .setMediaMetadata(
+                MediaMetadata.Builder()
+                    .setTitle(title)
+                    .setSubtitle(subtitle)
+                    .setArtworkUri(artworkUri)
+                    .build()
+            )
+            .build()
+    }
+}
