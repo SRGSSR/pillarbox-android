d1efdd0c11c425bd640a669dc80b7778b4bbf151
Joaquim Stähli <joaquim.staehli@gmail.com>
2023-09-26 10:51:21 +0200

    Update demo to target api 34 (android 14).

--- buildSrc/src/main/java/AppConfig.kt
+++ buildSrc/src/main/java/AppConfig.kt
@@ -6,7 +6,7 @@ import java.util.*
 
 object AppConfig {
     const val minSdk = 21
-    const val targetSdk = 33
+    const val targetSdk = 34
     const val compileSdk = 34
     // https://developer.android.com/jetpack/androidx/releases/compose-kotlin
     const val composeCompiler = "1.5.1"
