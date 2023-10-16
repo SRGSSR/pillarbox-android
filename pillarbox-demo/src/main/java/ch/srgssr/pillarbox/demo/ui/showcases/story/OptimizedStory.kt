b36fba3d262f2196a238d8ca5593d4b9e26c4bba
Joaquim Stähli <joaquim.staehli@gmail.com>
2023-10-16 10:12:08 +0200

    Add page indicator to story showcase

--- pillarbox-demo/src/main/java/ch/srgssr/pillarbox/demo/ui/showcases/story/OptimizedStory.kt
+++ pillarbox-demo/src/main/java/ch/srgssr/pillarbox/demo/ui/showcases/story/OptimizedStory.kt
@@ -7,7 +7,14 @@ package ch.srgssr.pillarbox.demo.ui.showcases.story
 import androidx.compose.animation.core.Spring
 import androidx.compose.animation.core.spring
 import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.pager.HorizontalPager
 import androidx.compose.foundation.pager.PagerDefaults
 import androidx.compose.foundation.pager.PagerSnapDistance
@@ -15,7 +22,10 @@ import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.DisposableEffect
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.drawBehind
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalLifecycleOwner
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.Lifecycle
@@ -54,36 +64,61 @@ fun OptimizedStory(storyViewModel: StoryViewModel = viewModel()) {
         }
     }
     val playlist = storyViewModel.playlist.items
-    HorizontalPager(
-        modifier = Modifier.fillMaxHeight(),
-        beyondBoundsPageCount = 0,
-        key = { page -> playlist[page].uri },
-        flingBehavior = PagerDefaults.flingBehavior(
-            state = pagerState,
-            pagerSnapDistance = PagerSnapDistance.atMost(0),
-            snapAnimationSpec = spring(stiffness = Spring.StiffnessHigh)
-        ),
-        pageSpacing = 1.dp,
-        state = pagerState
-    ) { page ->
-        // When flinging -> may "load" more that 3 pages
-        val currentPage = pagerState.currentPage
-        val player = if (page == currentPage - 1 || page == currentPage + 1 || page == currentPage) {
-            val playerConfig = storyViewModel.getPlayerAndMediaItemIndexForPage(page)
-            val playerPage = storyViewModel.getPlayerFromIndex(playerConfig.first)
-            playerPage.playWhenReady = currentPage == page
-            playerPage.seekToDefaultPosition(playerConfig.second)
-            playerPage
-        } else {
-            null
+    Box(modifier = Modifier.fillMaxSize()) {
+        HorizontalPager(
+            beyondBoundsPageCount = 0,
+            key = { page -> playlist[page].uri },
+            flingBehavior = PagerDefaults.flingBehavior(
+                state = pagerState,
+                pagerSnapDistance = PagerSnapDistance.atMost(0),
+                snapAnimationSpec = spring(stiffness = Spring.StiffnessHigh)
+            ),
+            pageSpacing = 1.dp,
+            state = pagerState
+        ) { page ->
+            // When flinging -> may "load" more that 3 pages
+            val currentPage = pagerState.currentPage
+            val player = if (page == currentPage - 1 || page == currentPage + 1 || page == currentPage) {
+                val playerConfig = storyViewModel.getPlayerAndMediaItemIndexForPage(page)
+                val playerPage = storyViewModel.getPlayerFromIndex(playerConfig.first)
+                playerPage.playWhenReady = currentPage == page
+                playerPage.seekToDefaultPosition(playerConfig.second)
+                playerPage
+            } else {
+                null
+            }
+            player?.let {
+                PlayerSurface(
+                    modifier = Modifier.fillMaxHeight(),
+                    scaleMode = ScaleMode.Crop,
+                    player = player,
+                )
+            }
+            Text(text = "Page $page")
         }
-        player?.let {
-            PlayerSurface(
-                modifier = Modifier.fillMaxHeight(),
-                scaleMode = ScaleMode.Crop,
-                player = player,
-            )
+        Row(
+            Modifier
+                .fillMaxWidth()
+                .align(Alignment.BottomCenter)
+                .padding(bottom = 12.dp),
+            horizontalArrangement = Arrangement.Center
+        ) {
+            repeat(playlist.size) { iteration ->
+                val color = if (pagerState.currentPage == iteration) ColorIndicatorCurrent else ColorIndicator
+                Box(
+                    modifier = Modifier
+                        .padding(2.dp)
+                        .size(IndicatorSize)
+                        .drawBehind {
+                            drawCircle(color)
+                        }
+
+                )
+            }
         }
-        Text(text = "Page $page")
     }
 }
+
+private val ColorIndicatorCurrent = Color.LightGray.copy(alpha = 0.75f)
+private val ColorIndicator = Color.LightGray.copy(alpha = 0.25f)
+private val IndicatorSize = 12.dp
