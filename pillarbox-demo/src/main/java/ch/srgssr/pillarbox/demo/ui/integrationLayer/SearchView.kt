5e842e6f565577afe461044e1515dd08e5c63c36
Gaëtan Muller <gaetan.muller@rts.ch>
2023-11-03 09:04:24 +0100

    Show/hide BU selector on scroll

--- pillarbox-demo/src/main/java/ch/srgssr/pillarbox/demo/ui/integrationLayer/SearchView.kt
+++ pillarbox-demo/src/main/java/ch/srgssr/pillarbox/demo/ui/integrationLayer/SearchView.kt
@@ -4,6 +4,7 @@
  */
 package ch.srgssr.pillarbox.demo.ui.integrationLayer
 
+import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
@@ -12,8 +13,10 @@ import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyListState
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Close
 import androidx.compose.material3.CircularProgressIndicator
@@ -28,8 +31,12 @@ import androidx.compose.material3.minimumInteractiveComponentSize
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.focus.FocusRequester
@@ -64,12 +71,16 @@ fun SearchView(searchViewModel: SearchViewModel, onSearchClicked: (Content.Media
     Column(
         verticalArrangement = Arrangement.spacedBy(8.dp)
     ) {
-        BuSelector(
-            modifier = Modifier.fillMaxWidth(),
-            listBu = bus,
-            selectedBu = currentBu,
-            onBuSelected = searchViewModel::selectBu
-        )
+        var showBuSelector by remember { mutableStateOf(true) }
+
+        AnimatedVisibility(visible = showBuSelector) {
+            BuSelector(
+                listBu = bus,
+                selectedBu = currentBu,
+                onBuSelected = searchViewModel::selectBu
+            )
+        }
+
         TextField(
             modifier = Modifier
                 .fillMaxWidth()
@@ -81,6 +92,13 @@ fun SearchView(searchViewModel: SearchViewModel, onSearchClicked: (Content.Media
                         imageVector = Icons.Default.Close,
                         contentDescription = "Clear search"
                     )
+                }
+            },
+            prefix = if (showBuSelector) {
+                null
+            } else {
+                {
+                    Text(text = "[${currentBu.name.uppercase()}] ")
                 }
             },
             singleLine = true,
@@ -102,6 +120,7 @@ fun SearchView(searchViewModel: SearchViewModel, onSearchClicked: (Content.Media
         SearchResultList(
             lazyPagingItems = lazyItems,
             contentClick = onSearchClicked,
+            onScroll = { showBuSelector = it },
             searchViewModel = searchViewModel,
             modifier = Modifier.padding(horizontal = 8.dp)
         )
@@ -117,13 +136,23 @@ private fun SearchResultList(
     searchViewModel: SearchViewModel,
     lazyPagingItems: LazyPagingItems<Content.Media>,
     contentClick: (Content.Media) -> Unit,
+    onScroll: (scrollUp: Boolean) -> Unit,
     modifier: Modifier = Modifier
 ) {
     val hasNoResult = lazyPagingItems.itemCount == 0 &&
         lazyPagingItems.loadState.refresh is LoadState.NotLoading &&
         searchViewModel.hasValidSearchQuery()
+    val scrollState = rememberLazyListState()
+    val isScrollingUp = scrollState.isScrollingUp()
 
-    LazyColumn(modifier = modifier) {
+    LaunchedEffect(isScrollingUp) {
+        onScroll(isScrollingUp)
+    }
+
+    LazyColumn(
+        modifier = modifier,
+        state = scrollState
+    ) {
         items(count = lazyPagingItems.itemCount, key = lazyPagingItems.itemKey()) { index ->
             val item = lazyPagingItems[index]
             item?.let { mediaResult ->
@@ -161,6 +190,27 @@ private fun SearchResultList(
             }
         }
     }
+}
+
+// Snippet from a Google Codelab:
+// https://github.com/android/codelab-android-compose/blob/main/AnimationCodelab/finished/src/main/java/com/example/android/codelab/animation/ui/home/Home.kt#L339
+@Composable
+private fun LazyListState.isScrollingUp(): Boolean {
+    var previousIndex by remember(this) { mutableIntStateOf(firstVisibleItemIndex) }
+    var previousScrollOffset by remember(this) { mutableIntStateOf(firstVisibleItemScrollOffset) }
+
+    return remember(this) {
+        derivedStateOf {
+            if (previousIndex != firstVisibleItemIndex) {
+                previousIndex > firstVisibleItemIndex
+            } else {
+                previousScrollOffset >= firstVisibleItemScrollOffset
+            }.also {
+                previousIndex = firstVisibleItemIndex
+                previousScrollOffset = firstVisibleItemScrollOffset
+            }
+        }
+    }.value
 }
 
 @Composable
