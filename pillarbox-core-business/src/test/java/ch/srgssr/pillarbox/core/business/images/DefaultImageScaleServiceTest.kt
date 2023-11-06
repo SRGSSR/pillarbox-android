d86e4fae9caf97c28290b8d736334d8ac0b176a6
Gaëtan Muller <gaetan.muller@rts.ch>
2023-11-06 11:30:31 +0100

    Test image service in different environments

--- pillarbox-core-business/src/test/java/ch/srgssr/pillarbox/core/business/images/DefaultImageScaleServiceTest.kt
+++ pillarbox-core-business/src/test/java/ch/srgssr/pillarbox/core/business/images/DefaultImageScaleServiceTest.kt
@@ -9,9 +9,11 @@ import org.junit.Test
 import org.junit.runner.RunWith
 import org.junit.runners.Parameterized
 import org.junit.runners.Parameterized.Parameters
+import java.net.URL
 
 @RunWith(Parameterized::class)
 class DefaultImageScaleServiceTest(
+    private val baseUrl: URL,
     private val imageUrl: String,
     private val width: ImageWidth,
     private val format: ImageFormat
@@ -20,14 +22,14 @@ class DefaultImageScaleServiceTest(
 
     @Before
     fun before() {
-        imageScaleService = DefaultImageScaleService(baseUrl = IlHost.PROD)
+        imageScaleService = DefaultImageScaleService(baseUrl)
     }
 
     @Test
     fun getScaledImageUrl() {
         val scaledImageUrl = imageScaleService.getScaledImageUrl(imageUrl, width, format)
 
-        assertEquals("https://il.srgssr.ch/images/?imageUrl=$imageUrl&format=${format.format}&width=${width.width}", scaledImageUrl)
+        assertEquals("${baseUrl}images/?imageUrl=$imageUrl&format=${format.format}&width=${width.width}", scaledImageUrl)
     }
 
     companion object {
@@ -35,9 +37,9 @@ class DefaultImageScaleServiceTest(
         @Parameters
         fun parameters(): Iterable<Any> {
             return listOf(
-                arrayOf("https://www.rts.ch/2020/05/18/14/20/11333286.image/16x9", ImageWidth.W240, ImageFormat.JPG),
-                arrayOf("https://www.rts.ch/2021/08/05/18/12/12396566.image/2x3", ImageWidth.W320, ImageFormat.PNG),
-                arrayOf("https://www.rts.ch/2022/10/06/17/32/13444418.image/4x5", ImageWidth.W480, ImageFormat.WEBP)
+                arrayOf(IlHost.PROD, "https://www.rts.ch/2020/05/18/14/20/11333286.image/16x9", ImageWidth.W240, ImageFormat.JPG),
+                arrayOf(IlHost.TEST, "https://www.rts.ch/2021/08/05/18/12/12396566.image/2x3", ImageWidth.W320, ImageFormat.PNG),
+                arrayOf(IlHost.STAGE, "https://www.rts.ch/2022/10/06/17/32/13444418.image/4x5", ImageWidth.W480, ImageFormat.WEBP)
             )
         }
     }
