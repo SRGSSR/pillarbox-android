6be9381043fd365e4a6711f59abe866dda9e304a
Gaëtan Muller <gaetan.muller@rts.ch>
2023-11-07 14:00:43 +0100

    Fix Unit Test to not depend on Android classes

--- pillarbox-core-business/src/test/java/ch/srgssr/pillarbox/core/business/MediaCompositionMediaItemSourceTest.kt
+++ pillarbox-core-business/src/test/java/ch/srgssr/pillarbox/core/business/MediaCompositionMediaItemSourceTest.kt
@@ -8,7 +8,7 @@ import androidx.media3.common.MediaItem
 import androidx.media3.common.MediaMetadata
 import ch.srgssr.pillarbox.core.business.exception.BlockReasonException
 import ch.srgssr.pillarbox.core.business.exception.ResourceNotFoundException
-import ch.srgssr.pillarbox.core.business.images.DefaultImageScalingService
+import ch.srgssr.pillarbox.core.business.images.ImageScalingService
 import ch.srgssr.pillarbox.core.business.integrationlayer.data.BlockReason
 import ch.srgssr.pillarbox.core.business.integrationlayer.data.Chapter
 import ch.srgssr.pillarbox.core.business.integrationlayer.data.MediaComposition
@@ -23,7 +23,7 @@ class MediaCompositionMediaItemSourceTest {
 
     private val mediaItemSource = MediaCompositionMediaItemSource(
         mediaCompositionDataSource = DummyMediaCompositionProvider(),
-        imageScalingService = DefaultImageScalingService()
+        imageScalingService = NoOpImageScalingService()
     )
 
     @Test(expected = IllegalArgumentException::class)
@@ -183,6 +183,12 @@ class MediaCompositionMediaItemSourceTest {
             fun createResource(type: Resource.Type): Resource {
                 return Resource(url = "", type = type)
             }
+        }
+    }
+
+    private class NoOpImageScalingService : ImageScalingService {
+        override fun getScaledImageUrl(imageUrl: String, width: ImageScalingService.ImageWidth, format: ImageScalingService.ImageFormat): String {
+            return imageUrl
         }
     }
 
