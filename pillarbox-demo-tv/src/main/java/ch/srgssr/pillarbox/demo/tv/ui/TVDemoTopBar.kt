4d58073979202adde176255531e5119aea8ccb6b
Gaëtan Muller <gaetan.muller@rts.ch>
2023-12-06 12:13:55 +0100

    Update `TVDemoTopBar` to use a `TabRow`

--- pillarbox-demo-tv/src/main/java/ch/srgssr/pillarbox/demo/tv/ui/TVDemoTopBar.kt
+++ pillarbox-demo-tv/src/main/java/ch/srgssr/pillarbox/demo/tv/ui/TVDemoTopBar.kt
@@ -5,26 +5,29 @@
 package ch.srgssr.pillarbox.demo.tv.ui
 
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.IntrinsicSize
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.focus.focusRestorer
+import androidx.compose.ui.focus.onFocusChanged
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.tv.material3.ExperimentalTvMaterial3Api
 import androidx.tv.material3.Icon
-import androidx.tv.material3.ListItem
-import androidx.tv.material3.ListItemDefaults
+import androidx.tv.material3.IconButton
 import androidx.tv.material3.MaterialTheme
+import androidx.tv.material3.Tab
+import androidx.tv.material3.TabRow
 import androidx.tv.material3.Text
 import ch.srgssr.pillarbox.demo.shared.ui.HomeDestination
 import ch.srgssr.pillarbox.demo.tv.ui.theme.PillarboxTheme
@@ -46,38 +49,50 @@ fun TVDemoTopBar(
     modifier: Modifier = Modifier,
     onDestinationClick: (destination: HomeDestination) -> Unit
 ) {
+    var focusedTabIndex by remember(selectedDestination) {
+        mutableIntStateOf(destinations.indexOf(selectedDestination))
+    }
+
     Row(
-        modifier = modifier
-            .fillMaxWidth()
-            .focusRestorer(),
-        horizontalArrangement = Arrangement.spacedBy(MaterialTheme.paddings.baseline),
+        modifier = modifier.fillMaxWidth(),
+        horizontalArrangement = Arrangement.SpaceBetween,
         verticalAlignment = Alignment.CenterVertically
     ) {
-        destinations.forEach { destination ->
-            ListItem(
-                selected = destination == selectedDestination,
-                onClick = { onDestinationClick(destination) },
-                modifier = Modifier.width(IntrinsicSize.Max),
-                headlineContent = {
-                    Text(text = stringResource(destination.labelResId))
+        TabRow(
+            selectedTabIndex = focusedTabIndex,
+            modifier = Modifier
+                .focusRestorer()
+                .onFocusChanged {
+                    if (!it.hasFocus) {
+                        focusedTabIndex = destinations.indexOf(selectedDestination)
+                    }
+                },
+        ) {
+            destinations.forEachIndexed { index, destination ->
+                Tab(
+                    selected = index == focusedTabIndex,
+                    onFocus = { focusedTabIndex = index },
+                    onClick = { onDestinationClick(destination) },
+                ) {
+                    Text(
+                        text = stringResource(destination.labelResId),
+                        modifier = Modifier.padding(
+                            horizontal = MaterialTheme.paddings.baseline,
+                            vertical = MaterialTheme.paddings.small,
+                        ),
+                    )
                 }
-            )
+            }
         }
 
-        Spacer(modifier = Modifier.weight(1f))
-
-        ListItem(
-            selected = selectedDestination == HomeDestination.Search,
+        IconButton(
             onClick = { onDestinationClick(HomeDestination.Search) },
-            modifier = Modifier.width(IntrinsicSize.Max),
-            shape = ListItemDefaults.shape(CircleShape),
-            headlineContent = {
-                Icon(
-                    imageVector = Icons.Default.Search,
-                    contentDescription = stringResource(HomeDestination.Search.labelResId)
-                )
-            }
-        )
+        ) {
+            Icon(
+                imageVector = Icons.Default.Search,
+                contentDescription = stringResource(HomeDestination.Search.labelResId),
+            )
+        }
     }
 }
 
