f5b2c734a2c49cbb8582a04a5ec8da944967e0cc
Gaëtan Muller <gaetan.muller@rts.ch>
2023-12-08 14:20:01 +0100

    Fix some Lint warnings

--- pillarbox-ui/src/main/java/ch/srgssr/pillarbox/ui/widget/KeepScreenOn.kt
+++ pillarbox-ui/src/main/java/ch/srgssr/pillarbox/ui/widget/KeepScreenOn.kt
@@ -4,6 +4,7 @@
  */
 package ch.srgssr.pillarbox.ui.widget
 
+import android.annotation.SuppressLint
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.platform.LocalView
@@ -14,6 +15,7 @@ import ch.srgssr.pillarbox.ui.extension.isPlayingAsState
  * Keep screen on when [Player.isPlaying]
  */
 @Composable
+@SuppressLint("ComposableNaming")
 fun Player.keepScreenOn() {
     val isPlaying by isPlayingAsState()
     LocalView.current.keepScreenOn = isPlaying
