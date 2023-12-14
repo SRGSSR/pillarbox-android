220aba9c0bfc9b25c5e450e6f8502d21c2f3a401
Gaëtan Muller <gaetan.muller@rts.ch>
2023-12-14 17:01:50 +0100

    Fix build and warning

--- pillarbox-demo/src/main/java/ch/srgssr/pillarbox/demo/ui/showcases/updatable/UpdatableMediaItemView.kt
+++ pillarbox-demo/src/main/java/ch/srgssr/pillarbox/demo/ui/showcases/updatable/UpdatableMediaItemView.kt
@@ -8,6 +8,7 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -22,7 +23,7 @@ import ch.srgssr.pillarbox.ui.widget.player.PlayerSurface
 fun UpdatableMediaItemView() {
     val updatableMediaItemViewModel: UpdatableMediaItemViewModel = viewModel()
     val player = updatableMediaItemViewModel.player
-    val currentItem = player.currentMediaMetadataAsState()
+    val currentItem by player.currentMediaMetadataAsState()
     PlayerSurface(player = player) {
         Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.TopStart) {
             Text(
