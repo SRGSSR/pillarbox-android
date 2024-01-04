7d29b7d9054b9b8ee0ae0149026e4c9155336bfc
Joaquim Stähli <joaquim.staehli@gmail.com>
2024-01-04 09:22:06 +0100

    Improve PillarboxLoadControl that shares Allocator

--- pillarbox-player/src/main/java/ch/srgssr/pillarbox/player/PillarboxLoadControl.kt
+++ pillarbox-player/src/main/java/ch/srgssr/pillarbox/player/PillarboxLoadControl.kt
@@ -4,6 +4,7 @@
  */
 package ch.srgssr.pillarbox.player
 
+import androidx.media3.common.C
 import androidx.media3.common.Timeline
 import androidx.media3.exoplayer.DefaultLoadControl
 import androidx.media3.exoplayer.LoadControl
@@ -12,41 +13,63 @@ import androidx.media3.exoplayer.source.MediaSource
 import androidx.media3.exoplayer.source.TrackGroupArray
 import androidx.media3.exoplayer.trackselection.ExoTrackSelection
 import androidx.media3.exoplayer.upstream.Allocator
+import androidx.media3.exoplayer.upstream.DefaultAllocator
+import kotlin.time.Duration
+import kotlin.time.Duration.Companion.milliseconds
 
 /**
- * Proxy [LoadControl] that will use a custom [LoadControl] if `smoothSeeking` is `true`, or the provided `defaultLoadControl` otherwise.
+ * Pillarbox [LoadControl] implementation that optimize content loading for smooth seeking.
  *
- * @param smoothSeeking `true` to use a custom [LoadControl] adapted for smooth seeking, `false` to use the provided [LoadControl].
- * @param defaultLoadControl The [LoadControl] to use if `smoothSeeking` is `false`.
+ * @param bufferDurations Buffer duration when [smoothSeeking] is not enabled.
+ * @property smoothSeeking if enabled use a optimized LoadControl.
+ * @param allocator The [DefaultAllocator] to use in internal DefaultLoadControl.
  */
 class PillarboxLoadControl(
-    smoothSeeking: Boolean = false,
-    defaultLoadControl: LoadControl = DefaultLoadControl()
+    bufferDurations: BufferDurations = BufferDurations(),
+    var smoothSeeking: Boolean = false,
+    private val allocator: DefaultAllocator = DefaultAllocator(true, C.DEFAULT_BUFFER_SEGMENT_SIZE),
 ) : LoadControl {
-    private val activeLoadControl: LoadControl = if (smoothSeeking) SmoothLoadControl() else defaultLoadControl
+
+    private val fastSeekLoadControl: DefaultLoadControl = DefaultLoadControl.Builder()
+        .setAllocator(allocator)
+        .setDurations(FAST_SEEK_DURATIONS)
+        .setPrioritizeTimeOverSizeThresholds(true)
+        .build()
+    private val defaultLoadControl: DefaultLoadControl = DefaultLoadControl.Builder()
+        .setAllocator(allocator)
+        .setDurations(bufferDurations)
+        .setPrioritizeTimeOverSizeThresholds(true)
+        .build()
+    private val activeLoadControl: LoadControl
+        get() {
+            return if (smoothSeeking) fastSeekLoadControl else defaultLoadControl
+        }
 
     override fun onPrepared() {
-        activeLoadControl.onPrepared()
+        fastSeekLoadControl.onPrepared()
+        defaultLoadControl.onPrepared()
     }
 
     override fun onStopped() {
-        activeLoadControl.onStopped()
+        fastSeekLoadControl.onStopped()
+        defaultLoadControl.onStopped()
     }
 
     override fun onReleased() {
-        activeLoadControl.onReleased()
+        fastSeekLoadControl.onReleased()
+        defaultLoadControl.onReleased()
     }
 
     override fun getAllocator(): Allocator {
-        return activeLoadControl.allocator
+        return allocator
     }
 
     override fun getBackBufferDurationUs(): Long {
-        return activeLoadControl.backBufferDurationUs
+        return BACK_BUFFER_DURATION_MS
     }
 
     override fun retainBackBufferFromKeyframe(): Boolean {
-        return activeLoadControl.retainBackBufferFromKeyframe()
+        return true
     }
 
     override fun shouldContinueLoading(
@@ -64,7 +87,8 @@ class PillarboxLoadControl(
         trackGroups: TrackGroupArray,
         trackSelections: Array<out ExoTrackSelection>
     ) {
-        activeLoadControl.onTracksSelected(timeline, mediaPeriodId, renderers, trackGroups, trackSelections)
+        fastSeekLoadControl.onTracksSelected(timeline, mediaPeriodId, renderers, trackGroups, trackSelections)
+        defaultLoadControl.onTracksSelected(timeline, mediaPeriodId, renderers, trackGroups, trackSelections)
     }
 
     @Deprecated("Deprecated in Java")
@@ -73,7 +97,8 @@ class PillarboxLoadControl(
         trackGroups: TrackGroupArray,
         trackSelections: Array<out ExoTrackSelection>
     ) {
-        activeLoadControl.onTracksSelected(renderers, trackGroups, trackSelections)
+        fastSeekLoadControl.onTracksSelected(renderers, trackGroups, trackSelections)
+        defaultLoadControl.onTracksSelected(renderers, trackGroups, trackSelections)
     }
 
     override fun shouldStartPlayback(
@@ -97,24 +122,39 @@ class PillarboxLoadControl(
         return activeLoadControl.shouldStartPlayback(bufferedDurationUs, playbackSpeed, rebuffering, targetLiveOffsetUs)
     }
 
-    private companion object SmoothLoadControl {
-        private const val MIN_BUFFER_MS = 2_000
-        private const val MAX_BUFFER_MS = 2_000
-        private const val BUFFER_FOR_PLAYBACK_MS = 2_000
-        private const val BUFFER_FOR_PLAYBACK_AFTER_REBUFFER_MS = 2_000
-        private const val BACK_BUFFER_DURATION_MS = 2_000
+    /**
+     * Buffer durations to use for [DefaultLoadControl.Builder.setBufferDurationsMs]
+     *
+     * @property minBufferDuration The minimum duration of media that the player will attempt to ensure is buffered at all times.
+     * @property maxBufferDuration The maximum duration of media that the player will attempt to buffer.
+     * @property bufferForPlayback The duration of media that must be buffered for playback to start or resume following a user action such as a seek.
+     * @property bufferForPlaybackAfterRebuffer The default duration of media that must be buffered for playback to resume after a rebuffer.
+     * A rebuffer is defined to be caused by buffer depletion rather than a user action.
+     * @constructor Create empty Buffer durations
+     */
+    data class BufferDurations(
+        val minBufferDuration: Duration = DefaultLoadControl.DEFAULT_MIN_BUFFER_MS.milliseconds,
+        val maxBufferDuration: Duration = DefaultLoadControl.DEFAULT_MAX_BUFFER_MS.milliseconds,
+        val bufferForPlayback: Duration = DefaultLoadControl.DEFAULT_BUFFER_FOR_PLAYBACK_MS.milliseconds,
+        val bufferForPlaybackAfterRebuffer: Duration = DefaultLoadControl.DEFAULT_BUFFER_FOR_PLAYBACK_AFTER_REBUFFER_MS.milliseconds,
+    )
 
-        operator fun invoke(): LoadControl {
-            return DefaultLoadControl.Builder()
-                .setPrioritizeTimeOverSizeThresholds(true)
-                .setBufferDurationsMs(
-                    MIN_BUFFER_MS,
-                    MAX_BUFFER_MS,
-                    BUFFER_FOR_PLAYBACK_MS,
-                    BUFFER_FOR_PLAYBACK_AFTER_REBUFFER_MS
-                )
-                .setBackBuffer(BACK_BUFFER_DURATION_MS, true)
-                .build()
+    private companion object SmoothLoadControl {
+        private const val BACK_BUFFER_DURATION_MS = 6_000L
+        private val FAST_SEEK_DURATIONS = BufferDurations(
+            minBufferDuration = 2_000.milliseconds,
+            maxBufferDuration = 2_000.milliseconds,
+            bufferForPlayback = 2_000.milliseconds,
+            bufferForPlaybackAfterRebuffer = 2_000.milliseconds,
+        )
+
+        private fun DefaultLoadControl.Builder.setDurations(durations: BufferDurations): DefaultLoadControl.Builder {
+            return setBufferDurationsMs(
+                durations.minBufferDuration.inWholeMilliseconds.toInt(),
+                durations.maxBufferDuration.inWholeMilliseconds.toInt(),
+                durations.bufferForPlayback.inWholeMilliseconds.toInt(),
+                durations.bufferForPlaybackAfterRebuffer.inWholeMilliseconds.toInt(),
+            )
         }
     }
 }
