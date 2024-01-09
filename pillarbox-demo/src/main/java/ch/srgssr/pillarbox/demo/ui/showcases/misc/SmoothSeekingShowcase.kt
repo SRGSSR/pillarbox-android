7281397c2e4bc947ef7f1d2037f8b7677ec8e6af
Joaquim Stähli <joaquim.staehli@gmail.com>
2024-01-09 16:59:36 +0100

    Update smooth seeking show case

--- pillarbox-demo/src/main/java/ch/srgssr/pillarbox/demo/ui/showcases/misc/SmoothSeekingShowcase.kt
+++ pillarbox-demo/src/main/java/ch/srgssr/pillarbox/demo/ui/showcases/misc/SmoothSeekingShowcase.kt
@@ -8,7 +8,6 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
@@ -17,6 +16,7 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Switch
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -24,11 +24,14 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.lifecycle.compose.LifecycleStartEffect
-import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.media3.common.Player
+import ch.srgssr.pillarbox.core.business.DefaultPillarbox
 import ch.srgssr.pillarbox.demo.R
+import ch.srgssr.pillarbox.demo.shared.data.DemoItem
+import ch.srgssr.pillarbox.demo.shared.di.PlayerModule
 import ch.srgssr.pillarbox.demo.ui.player.controls.PlayerPlaybackRow
 import ch.srgssr.pillarbox.demo.ui.player.controls.PlayerTimeSlider
 import ch.srgssr.pillarbox.demo.ui.player.controls.rememberProgressTrackerState
@@ -42,17 +45,35 @@ import ch.srgssr.pillarbox.ui.widget.player.PlayerSurface
  */
 @Composable
 fun SmoothSeekingShowcase() {
-    val smoothSeekingViewModel: SmoothSeekingViewModel = viewModel()
-    val player = smoothSeekingViewModel.player
+    val context = LocalContext.current
+    val player = remember {
+        DefaultPillarbox(
+            context = context,
+            mediaItemSource = PlayerModule.provideMixedItemSource(context)
+        ).apply {
+            addMediaItem(DemoItem.UnifiedStreamingOnDemand_Dash_TrickPlay.toMediaItem())
+            addMediaItem(DemoItem.UnifiedStreamingOnDemandTrickplay.toMediaItem())
+            addMediaItem(DemoItem.UnifiedStreamingOnDemand_Dash_FragmentedMP4.toMediaItem())
+            addMediaItem(DemoItem.OnDemandHLS.toMediaItem())
+            addMediaItem(DemoItem.GoogleDashH265.toMediaItem())
+        }
+    }
+    DisposableEffect(Unit) {
+        player.prepare()
+        player.play()
+        onDispose {
+            player.release()
+        }
+    }
     var smoothSeekingEnabled by remember {
         mutableStateOf(false)
     }
 
     Column {
-        Box(modifier = Modifier.aspectRatio(16 / 9f)) {
+        Box(modifier = Modifier) {
             val playbackState by player.playbackStateAsState()
             val isBuffering = playbackState == Player.STATE_BUFFERING
-            PlayerSurface(player = player) {
+            PlayerSurface(player = player, defaultAspectRatio = 16 / 9f) {
                 if (isBuffering) {
                     Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
                         CircularProgressIndicator(modifier = Modifier.align(Alignment.Center), color = Color.White)
@@ -86,7 +107,6 @@ fun SmoothSeekingShowcase() {
                 checked = smoothSeekingEnabled,
                 onCheckedChange = { enabled ->
                     smoothSeekingEnabled = enabled
-                    smoothSeekingViewModel.setSmoothSeekingEnabled(enabled)
                 }
             )
 
