f686a0355e6d273507eca19a81653eb2febf0271
Gaëtan Muller <gaetan.muller@rts.ch>
2024-03-19 15:05:15 +0100

    Migrate Android Tests to Unit Tests

--- pillarbox-player/build.gradle.kts
+++ pillarbox-player/build.gradle.kts
@@ -14,16 +14,6 @@ android {
     buildFeatures {
         buildConfig = true
     }
-
-    // Mockk includes some licenses information, which may conflict with other license files. This block merges all licenses together.
-    // Mockk excludes all licenses instead:
-    // https://github.com/mockk/mockk/blob/f879502a044c83c2a5fd52992f20903209eb34f3/modules/mockk-android/build.gradle.kts#L14-L19
-    packaging {
-        resources {
-            merges += "META-INF/LICENSE.md"
-            merges += "META-INF/LICENSE-notice.md"
-        }
-    }
 }
 
 dependencies {
@@ -53,14 +43,8 @@ dependencies {
     testImplementation(libs.kotlinx.coroutines.test)
     testImplementation(libs.mockk)
     testImplementation(libs.mockk.dsl)
-    testRuntimeOnly(libs.robolectric)
+    testImplementation(libs.robolectric)
     testImplementation(libs.robolectric.annotations)
     testImplementation(libs.robolectric.shadows.framework)
     testImplementation(libs.turbine)
-
-    androidTestImplementation(libs.androidx.test.monitor)
-    androidTestRuntimeOnly(libs.androidx.test.runner)
-    androidTestImplementation(libs.junit)
-    androidTestRuntimeOnly(libs.kotlinx.coroutines.android)
-    androidTestImplementation(libs.mockk)
 }
