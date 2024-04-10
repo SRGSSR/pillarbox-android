2890d34c650a59f55b3e6c1077347c6aa52a17bd
Gaëtan Muller <gaetan.muller@rts.ch>
2024-04-10 17:10:14 +0200

    Create `Track` models

---
+++ pillarbox-player/src/main/java/ch/srgssr/pillarbox/player/tracks/Track.kt
@@ -0,0 +1,76 @@
+/*
+ * Copyright (c) SRG SSR. All rights reserved.
+ * License information is available from the LICENSE file.
+ */
+package ch.srgssr.pillarbox.player.tracks
+
+import androidx.media3.common.C.TRACK_TYPE_AUDIO
+import androidx.media3.common.C.TRACK_TYPE_TEXT
+import androidx.media3.common.C.TRACK_TYPE_VIDEO
+import androidx.media3.common.Format
+import androidx.media3.common.Tracks
+
+sealed class Track(
+    val format: Format,
+    internal val groupIndex: Int,
+    internal val trackIndexInGroup: Int,
+    internal val isSupported: Boolean,
+) {
+    companion object {
+        operator fun invoke(
+            group: Tracks.Group,
+            groupIndex: Int,
+            trackIndexInGroup: Int,
+        ): Track {
+            val trackConstructor = when (val trackType = group.type) {
+                TRACK_TYPE_AUDIO -> ::AudioTrack
+                TRACK_TYPE_TEXT -> ::TextTrack
+                TRACK_TYPE_VIDEO -> ::VideoTrack
+                else -> error("Unsupported track type $trackType")
+            }
+
+            return trackConstructor(
+                group.getTrackFormat(trackIndexInGroup),
+                groupIndex,
+                trackIndexInGroup,
+                group.isTrackSupported(trackIndexInGroup),
+            )
+        }
+    }
+}
+
+class AudioTrack(
+    format: Format,
+    groupIndex: Int,
+    trackIndexInGroup: Int,
+    isSupported: Boolean,
+) : Track(
+    format = format,
+    groupIndex = groupIndex,
+    trackIndexInGroup = trackIndexInGroup,
+    isSupported = isSupported,
+)
+
+class TextTrack(
+    format: Format,
+    groupIndex: Int,
+    trackIndexInGroup: Int,
+    isSupported: Boolean,
+) : Track(
+    format = format,
+    groupIndex = groupIndex,
+    trackIndexInGroup = trackIndexInGroup,
+    isSupported = isSupported,
+)
+
+class VideoTrack(
+    format: Format,
+    groupIndex: Int,
+    trackIndexInGroup: Int,
+    isSupported: Boolean,
+) : Track(
+    format = format,
+    groupIndex = groupIndex,
+    trackIndexInGroup = trackIndexInGroup,
+    isSupported = isSupported,
+)
