c3555afe4761540b9a3eeccb7d6b29953e008b3a
Gaëtan Muller <gaetan.muller@rts.ch>
2024-04-10 08:44:08 +0200

    Rework `LiveIndicator`

--- pillarbox-demo/src/main/java/ch/srgssr/pillarbox/demo/ui/player/LiveIndicator.kt
+++ pillarbox-demo/src/main/java/ch/srgssr/pillarbox/demo/ui/player/LiveIndicator.kt
@@ -4,23 +4,23 @@
  */
 package ch.srgssr.pillarbox.demo.ui.player
 
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.drawBehind
-import androidx.compose.ui.geometry.CornerRadius
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.semantics.Role
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
+import ch.srgssr.pillarbox.demo.ui.theme.PillarboxTheme
+import ch.srgssr.pillarbox.demo.ui.theme.paddings
 
 /**
  * Display a live indicator.
@@ -36,17 +36,25 @@ fun LiveIndicator(
     modifier: Modifier = Modifier,
     onClick: () -> Unit = {},
 ) {
+    val backgroundColor by animateColorAsState(targetValue = if (isAtLive) Color.Red else Color.Gray)
+
     Box(
-        modifier = modifier.clickable(enabled = !isAtLive, onClick = onClick, role = Role.Button, onClickLabel = "back to live"),
-        contentAlignment = Alignment.Center
+        modifier = modifier
+            .clickable(
+                enabled = !isAtLive,
+                onClick = onClick,
+                role = Role.Button,
+                onClickLabel = "Back to live",
+            )
+            .background(
+                color = backgroundColor,
+                shape = MaterialTheme.shapes.small,
+            ),
     ) {
         Text(
-            modifier = Modifier.drawBehind {
-                this.drawRoundRect(color = if (isAtLive) Color.Red else Color.Gray, cornerRadius = CornerRadius(10f))
-            }.wrapContentHeight(align = Alignment.CenterVertically),
-            textAlign = TextAlign.Center,
-            text = " LIVE ",
-            color = Color.White
+            text = "LIVE",
+            modifier = Modifier.padding(horizontal = MaterialTheme.paddings.small),
+            color = Color.White,
         )
     }
 }
@@ -54,12 +62,11 @@ fun LiveIndicator(
 @Preview(showBackground = false)
 @Composable
 private fun LiveIndicatorPreview() {
-    MaterialTheme {
-        Column(modifier = Modifier, verticalArrangement = Arrangement.spacedBy(12.dp)) {
+    PillarboxTheme {
+        Column(verticalArrangement = Arrangement.spacedBy(MaterialTheme.paddings.baseline)) {
             LiveIndicator(isAtLive = true)
-            LiveIndicator(
-                isAtLive = false
-            )
+
+            LiveIndicator(isAtLive = false)
         }
     }
 }
