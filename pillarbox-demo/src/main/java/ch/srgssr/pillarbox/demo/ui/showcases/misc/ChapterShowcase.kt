71d9a7a3030907adee25ed02523b39f885a33a69
Joaquim Stähli <joaquim.staehli@gmail.com>
2024-04-25 12:04:01 +0200

    Update chapter list

--- pillarbox-demo/src/main/java/ch/srgssr/pillarbox/demo/ui/showcases/misc/ChapterShowcase.kt
+++ pillarbox-demo/src/main/java/ch/srgssr/pillarbox/demo/ui/showcases/misc/ChapterShowcase.kt
@@ -7,12 +7,10 @@ package ch.srgssr.pillarbox.demo.ui.showcases.misc
 import android.content.res.Configuration
 import android.net.Uri
 import androidx.compose.animation.AnimatedVisibility
-import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -23,7 +21,7 @@ import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.ImageNotSupported
+import androidx.compose.material.icons.filled.Image
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -32,14 +30,19 @@ import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.scale
+import androidx.compose.ui.graphics.BlendMode
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.graphics.vector.rememberVectorPainter
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalConfiguration
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextOverflow
-import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.zIndex
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.media3.common.MediaMetadata
 import ch.srgssr.pillarbox.demo.ui.player.PlayerView
@@ -63,17 +66,19 @@ fun ChapterShowcase() {
             modifier = Modifier
                 .fillMaxWidth()
                 .wrapContentHeight()
-                .weight(4f),
+                .weight(1f),
             player = showCaseViewModel.player,
             progressTracker = showCaseViewModel.progressTracker
         )
         AnimatedVisibility(
             modifier = Modifier
                 .fillMaxWidth()
-                .weight(1f),
+                .height(160.dp),
             visible = chapters.isNotEmpty() && configuration.orientation != Configuration.ORIENTATION_LANDSCAPE
         ) {
             ChapterList(
+                modifier = Modifier
+                    .padding(8.dp),
                 chapters = chapters,
                 currentChapter = currentChapter,
                 onChapterClicked = showCaseViewModel::chapterClicked,
@@ -91,24 +96,22 @@ private fun ChapterList(
     currentChapter: Chapter? = null,
     onChapterClicked: (Chapter) -> Unit = {}
 ) {
-    val currentIndex = currentChapter?.let { chapters.indexOf(it) } ?: 0
-    val state = rememberLazyListState(currentIndex, CurrentItemOffset)
+    val currentIndex = currentChapter?.let { chapters.indexOf(it) } ?: -1
+    val state = rememberLazyListState(if (currentIndex >= 0) currentIndex else 0, CurrentItemOffset)
     LaunchedEffect(currentIndex, chapters) {
-        currentChapter?.let {
-            state.animateScrollToItem(currentIndex, CurrentItemOffset)
+        if (currentIndex >= 0) {
+            state.animateScrollToItem(currentIndex, if (currentIndex != 0) CurrentItemOffset else 0)
         }
     }
     LazyRow(
         modifier = modifier,
-        horizontalArrangement = Arrangement.spacedBy(MaterialTheme.paddings.mini),
-        contentPadding = PaddingValues(MaterialTheme.paddings.mini),
+        horizontalArrangement = Arrangement.spacedBy(MaterialTheme.paddings.baseline),
         state = state
     ) {
         items(items = chapters, key = { it.id }) { chapter ->
             ChapterItem(
                 modifier = Modifier
-                    .fillParentMaxHeight()
-                    .aspectRatio(16 / 10f),
+                    .aspectRatio(16 / 9f),
                 chapter = chapter, active = currentChapter == chapter, onClick = { onChapterClicked(chapter) }
             )
         }
@@ -124,29 +127,34 @@ private fun ChapterItem(
 ) {
     Box(
         modifier = modifier
+            .zIndex(if (active) 1f else 0f)
+            .scale(if (active) 1.05f else 1f)
             .clickable { onClick() },
         contentAlignment = Alignment.Center
     ) {
-        val placeholder = rememberVectorPainter(image = Icons.Default.ImageNotSupported)
+        val placeholder = rememberVectorPainter(image = Icons.Default.Image)
         AsyncImage(
             model = chapter.mediaMetadata.artworkUri,
             contentDescription = "",
             placeholder = placeholder,
             fallback = placeholder,
             contentScale = ContentScale.Fit,
+            // alpha =  if(active) 0.8f else 0.5f, //or
+            colorFilter = ColorFilter.tint(color = Color.Black.copy(alpha = if (active) 0.2f else 0.5f), blendMode = BlendMode.SrcOver),
             modifier = Modifier.fillMaxSize(),
         )
         Text(
             modifier = Modifier
+                .align(Alignment.BottomStart)
                 .fillMaxWidth()
-                .background(color = if (active) Color.Red else Color.Black)
-                .padding(MaterialTheme.paddings.micro)
-                .align(Alignment.BottomStart),
+                .padding(MaterialTheme.paddings.small),
             maxLines = 2,
             minLines = 2,
+            textAlign = TextAlign.Start,
             overflow = TextOverflow.Ellipsis,
             text = chapter.mediaMetadata.title.toString(),
             style = MaterialTheme.typography.bodySmall,
+            fontWeight = if (active) FontWeight.Bold else null,
             color = Color.White,
         )
     }
@@ -156,70 +164,24 @@ private fun ChapterItem(
 @Composable
 private fun ChapterItemPreview() {
     MaterialTheme {
-        Column(modifier = Modifier.fillMaxWidth()) {
-            val modifier = Modifier
-                .fillMaxWidth(0.75f)
-                .aspectRatio(16 / 9f)
-            ChapterItem(
-                modifier = modifier,
-                chapter = Chapter(
-                    "i1", 5.minutes.inWholeMilliseconds, 10.minutes.inWholeMilliseconds,
-                    MediaMetadata.Builder()
-                        .setTitle("Title1")
-                        .build()
-                ),
-                active = true,
-                onClick = {}
-            )
-            ChapterItem(
-                modifier = modifier,
-                chapter = Chapter(
-                    "i1", 5.minutes.inWholeMilliseconds, 12.minutes.inWholeMilliseconds,
-                    MediaMetadata.Builder()
-                        .setTitle("Title2")
-                        .setArtworkUri(
-                            Uri.parse(
-                                "https://cdn.prod.swi-services" +
-                                    ".ch/video-delivery/images/14e4562f-725d-4e41-a200-7fcaa77df2fe/5rwf1Bq_m3GC5secOZcIcgbbrbZPf4nI/16x9"
-                            )
-                        )
-                        .build()
-                ),
-                active = false,
-                onClick = {}
-            )
-        }
-    }
-}
-
-@Preview(showBackground = true, device = Devices.NEXUS_5)
-@Composable
-private fun ChapterListPreview() {
-    val listChapters = listOf(
-        Chapter(
-            "i1", 5.minutes.inWholeMilliseconds, 10.minutes.inWholeMilliseconds,
-            MediaMetadata.Builder()
-                .setTitle("Title1")
-                .build()
-        ),
-        Chapter(
-            "i2", 5.minutes.inWholeMilliseconds, 10.minutes.inWholeMilliseconds,
-            MediaMetadata.Builder()
-                .setTitle("Title 2 that can be a long as needed but shorter as required!")
-                .build()
-        ),
-        Chapter(
-            "i3", 5.minutes.inWholeMilliseconds, 10.minutes.inWholeMilliseconds,
-            MediaMetadata.Builder()
-                .setTitle("Title 3")
-                .build()
-        ),
-    )
-    MaterialTheme {
-        ChapterList(
+        ChapterItem(
             modifier = Modifier
-                .height(150.dp),
-            chapters = listChapters
+                .fillMaxWidth()
+                .aspectRatio(16 / 9f),
+            chapter = Chapter(
+                "i1", 5.minutes.inWholeMilliseconds, 12.minutes.inWholeMilliseconds,
+                MediaMetadata.Builder()
+                    .setTitle("Title2")
+                    .setArtworkUri(
+                        Uri.parse(
+                            "https://cdn.prod.swi-services" +
+                                ".ch/video-delivery/images/14e4562f-725d-4e41-a200-7fcaa77df2fe/5rwf1Bq_m3GC5secOZcIcgbbrbZPf4nI/16x9"
+                        )
+                    )
+                    .build()
+            ),
+            active = false,
+            onClick = {}
         )
     }
 }
