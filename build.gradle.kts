5630b906f262faf036cbcf512cb18e4fcf1446b5
Gaëtan Muller <gaetan.muller@rts.ch>
2024-05-03 14:13:57 +0200

    Make the Dependency Analysis plugin only fails for unused dependencies (#526)

--- build.gradle.kts
+++ build.gradle.kts
@@ -80,15 +80,12 @@ tasks.getByPath(":pillarbox-demo:preBuild").dependsOn(":installGitHook")
 dependencyAnalysis {
     issues {
         all {
-            onAny {
+            onUnusedDependencies {
                 severity("fail")
             }
         }
 
         structure {
-            // https://github.com/autonomousapps/dependency-analysis-gradle-plugin/wiki/Customizing-plugin-behavior
-            ignoreKtx(true) // default is false
-
             // Required because of https://github.com/autonomousapps/dependency-analysis-gradle-plugin/issues/892
             bundle("kotlin-test") {
                 includeDependency(libs.kotlin.test)
