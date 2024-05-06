dd8531eba9dc9d0b540df13102dc25e91b9bb21f
Gaëtan Muller <gaetan.muller@rts.ch>
2024-05-06 10:52:24 +0200

    Integrate Dokka (#529)

--- build-logic/plugins/src/main/java/ch/srgssr/pillarbox/gradle/PillarboxAndroidLibraryPublishingPlugin.kt
+++ build-logic/plugins/src/main/java/ch/srgssr/pillarbox/gradle/PillarboxAndroidLibraryPublishingPlugin.kt
@@ -10,6 +10,7 @@ import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.gradle.api.publish.PublishingExtension
 import org.gradle.api.publish.maven.MavenPublication
+import org.gradle.jvm.tasks.Jar
 import org.gradle.kotlin.dsl.configure
 import org.gradle.kotlin.dsl.get
 import org.gradle.kotlin.dsl.register
@@ -21,6 +22,23 @@ class PillarboxAndroidLibraryPublishingPlugin : Plugin<Project> {
     override fun apply(target: Project) = with(target) {
         pluginManager.apply("com.android.library")
         pluginManager.apply("org.gradle.maven-publish")
+        pluginManager.apply("org.jetbrains.dokka")
+
+        val dokkaHtmlJar = tasks.register<Jar>("dokkaHtmlJar") {
+            val dokkaHtmlTask = tasks.named("dokkaHtml")
+
+            dependsOn(dokkaHtmlTask)
+            from(dokkaHtmlTask.map { it.outputs })
+            archiveClassifier.set("html-docs")
+        }
+
+        val dokkaJavadocJar = tasks.register<Jar>("dokkaJavadocJar") {
+            val dokkaJavadocTask = tasks.named("dokkaJavadoc")
+
+            dependsOn(dokkaJavadocTask)
+            from(dokkaJavadocTask.map { it.outputs })
+            archiveClassifier.set("javadoc")
+        }
 
         extensions.configure<LibraryExtension> {
             defaultConfig {
@@ -31,7 +49,6 @@ class PillarboxAndroidLibraryPublishingPlugin : Plugin<Project> {
             publishing {
                 singleVariant("release") {
                     withSourcesJar()
-                    withJavadocJar()
                 }
             }
         }
@@ -39,6 +56,9 @@ class PillarboxAndroidLibraryPublishingPlugin : Plugin<Project> {
         extensions.configure<PublishingExtension> {
             publications {
                 register<MavenPublication>("gpr") {
+                    artifact(dokkaHtmlJar)
+                    artifact(dokkaJavadocJar)
+
                     afterEvaluate {
                         from(components["release"])
                     }
