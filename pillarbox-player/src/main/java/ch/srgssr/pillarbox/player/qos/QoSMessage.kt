4e0288d4a75ee2d12c1f8aa014bfa68773f96fa6
Gaëtan Muller <gaetan.muller@rts.ch>
2024-06-21 11:17:02 +0200

    Add models for QoS and an `AnalyticsListener` to gather session load times (#596)
    Co-authored-by: Joaquim Stähli <joaquim.staehli@gmail.com>

---
+++ pillarbox-player/src/main/java/ch/srgssr/pillarbox/player/qos/QoSMessage.kt
@@ -0,0 +1,20 @@
+/*
+ * Copyright (c) SRG SSR. All rights reserved.
+ * License information is available from the LICENSE file.
+ */
+package ch.srgssr.pillarbox.player.qos
+
+/**
+ * Represents a QoS message.
+ *
+ * @property data The data associated with the message.
+ * @property eventName The name of the event.
+ * @property sessionId The session id.
+ * @property timestamp The current timestamp.
+ */
+data class QoSMessage(
+    val data: Any,
+    val eventName: String,
+    val sessionId: String,
+    val timestamp: Long = System.currentTimeMillis(),
+)
--- pillarbox-player/src/main/java/ch/srgssr/pillarbox/player/source/PillarboxMediaSourceFactory.kt
+++ pillarbox-player/src/main/java/ch/srgssr/pillarbox/player/source/PillarboxMediaSourceFactory.kt
@@ -12,14 +12,19 @@ import androidx.media3.exoplayer.source.MediaSource
 import androidx.media3.exoplayer.upstream.LoadErrorHandlingPolicy
 import ch.srgssr.pillarbox.player.asset.AssetLoader
 import ch.srgssr.pillarbox.player.asset.UrlAssetLoader
+import kotlin.time.TimeSource
 
 /**
  * Pillarbox media source factory create a new [PillarboxMediaSource] from a [MediaItem].
  * It selects the first [AssetLoader] to use by checking if [AssetLoader.canLoadAsset].
  *
  * @param context to create the [defaultAssetLoader].
+ * @param timeSource The [TimeSource].
  */
-class PillarboxMediaSourceFactory(context: Context) : MediaSource.Factory {
+class PillarboxMediaSourceFactory(
+    context: Context,
+    private val timeSource: TimeSource = TimeSource.Monotonic
+) : MediaSource.Factory {
     /**
      * Default asset loader used when no other AssetLoader has been found.
      */
@@ -75,7 +80,12 @@ class PillarboxMediaSourceFactory(context: Context) : MediaSource.Factory {
     override fun createMediaSource(mediaItem: MediaItem): MediaSource {
         checkNotNull(mediaItem.localConfiguration)
         val assetLoader = listAssetLoader.firstOrNull { it.canLoadAsset(mediaItem) } ?: defaultAssetLoader
-        return PillarboxMediaSource(mediaItem = mediaItem, assetLoader = assetLoader, minLiveDvrDurationMs = minLiveDvrDurationMs)
+        return PillarboxMediaSource(
+            mediaItem = mediaItem,
+            assetLoader = assetLoader,
+            minLiveDvrDurationMs = minLiveDvrDurationMs,
+            timeSource = timeSource
+        )
     }
 
     companion object {
