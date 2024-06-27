b9676c98905882d1cd6dc39e7a0eb602954b62a3
Gaëtan Muller <gaetan.muller@rts.ch>
2024-06-27 09:36:36 +0200

    Link `PlaybackSessionManager` with `QoSSessionAnalyticsListener` (#609)

--- pillarbox-player/src/test/java/ch/srgssr/pillarbox/player/qos/QoSSessionTimingsTest.kt
+++ pillarbox-player/src/test/java/ch/srgssr/pillarbox/player/qos/QoSSessionTimingsTest.kt
@@ -7,7 +7,6 @@ package ch.srgssr.pillarbox.player.qos
 import kotlin.test.Test
 import kotlin.test.assertEquals
 import kotlin.time.Duration
-import kotlin.time.Duration.Companion.milliseconds
 
 class QoSSessionTimingsTest {
     @Test
@@ -15,19 +14,8 @@ class QoSSessionTimingsTest {
         val timings = QoSSessionTimings.Zero
 
         assertEquals(Duration.ZERO, timings.asset)
+        assertEquals(Duration.ZERO, timings.currentToStart)
         assertEquals(Duration.ZERO, timings.drm)
         assertEquals(Duration.ZERO, timings.mediaSource)
-        assertEquals(Duration.ZERO, timings.total)
-    }
-
-    @Test
-    fun `timings compute total value`() {
-        val timings = QoSSessionTimings(
-            asset = 250.milliseconds,
-            drm = 33.milliseconds,
-            mediaSource = 100.milliseconds,
-        )
-
-        assertEquals(383.milliseconds, timings.total)
     }
 }
