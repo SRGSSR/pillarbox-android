9cdde806bd742acbd9404cf0c5fb066d152a731b
Gaëtan Muller <m.gaetan89@gmail.com>
2024-07-12 15:07:59 +0200

    Improve sliders a bit

--- pillarbox-demo/src/main/java/ch/srgssr/pillarbox/demo/ui/showcases/misc/ResizablePlayerShowcase.kt
+++ pillarbox-demo/src/main/java/ch/srgssr/pillarbox/demo/ui/showcases/misc/ResizablePlayerShowcase.kt
@@ -16,7 +16,6 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.systemGestureExclusion
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.SegmentedButton
 import androidx.compose.material3.SegmentedButtonDefaults
@@ -73,7 +72,6 @@ fun ResizablePlayerShowcase() {
 }
 
 @Composable
-@OptIn(ExperimentalMaterial3Api::class)
 private fun AdaptivePlayer(player: Player, modifier: Modifier = Modifier) {
     var resizeMode by remember { mutableStateOf(ScaleMode.Fit) }
     val (widthPercent, setWidthPercent) = remember { mutableFloatStateOf(1f) }
