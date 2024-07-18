7300afc68f6f8f2a90ea70dbc4ea0d83dc0346b2
Joaquim Stähli <joaquim.staehli@gmail.com>
2024-07-18 08:37:08 +0200

    631 metrics collector (#638)
    Co-authored-by: Gaëtan Muller <gaetan.muller@rts.ch>
    Co-authored-by: Gaëtan Muller <m.gaetan89@gmail.com>

---
+++ pillarbox-player/src/main/java/ch/srgssr/pillarbox/player/analytics/extension/EventTime.kt
@@ -0,0 +1,13 @@
+/*
+ * Copyright (c) SRG SSR. All rights reserved.
+ * License information is available from the LICENSE file.
+ */
+package ch.srgssr.pillarbox.player.analytics.extension
+
+import androidx.media3.common.Timeline.Window
+import androidx.media3.exoplayer.analytics.AnalyticsListener.EventTime
+
+internal fun EventTime.getUidOfPeriod(window: Window): Any {
+    timeline.getWindow(windowIndex, window)
+    return timeline.getUidOfPeriod(window.firstPeriodIndex)
+}
