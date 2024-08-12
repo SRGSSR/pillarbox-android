dbd3a9e8390cfc04a2a6cb664137b932dced1dbb
Gaëtan Muller <gaetan.muller@rts.ch>
2024-08-12 13:54:39 +0200

    Display metrics overlay in the TV demo and add a dedicated setting (#666)

--- pillarbox-demo-shared/src/main/java/ch/srgssr/pillarbox/demo/shared/ui/player/metrics/MetricsOverlay.kt
+++ pillarbox-demo-shared/src/main/java/ch/srgssr/pillarbox/demo/shared/ui/player/metrics/MetricsOverlay.kt
@@ -2,16 +2,15 @@
  * Copyright (c) SRG SSR. All rights reserved.
  * License information is available from the LICENSE file.
  */
-package ch.srgssr.pillarbox.demo.ui.player.metrics
+package ch.srgssr.pillarbox.demo.shared.ui.player.metrics
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.text.BasicText
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.graphics.Shadow
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.sp
 import androidx.media3.common.Format
 import ch.srgssr.pillarbox.demo.shared.ui.settings.MetricsOverlayOptions
 import ch.srgssr.pillarbox.player.analytics.metrics.PlaybackMetrics
@@ -108,7 +107,12 @@ private fun OverlayText(
 ) {
     BasicText(
         modifier = modifier,
-        style = TextStyle.Default.copy(fontSize = overlayOptions.textSize),
+        style = overlayOptions.textStyle.copy(
+            shadow = Shadow(
+                color = Color.Black,
+                blurRadius = 4f,
+            ),
+        ),
         color = { overlayOptions.textColor },
         text = text,
     )
@@ -117,7 +121,7 @@ private fun OverlayText(
 @Preview
 @Composable
 private fun OverlayTextPreview() {
-    val overlayOptions = MetricsOverlayOptions(textColor = Color.Yellow, textSize = 12.sp)
+    val overlayOptions = MetricsOverlayOptions()
     OverlayText(text = "Text; 12 ac1.mp3 channels:4 colors:4", overlayOptions = overlayOptions)
 }
 
