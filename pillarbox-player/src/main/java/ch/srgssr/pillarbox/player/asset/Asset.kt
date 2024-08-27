0b521e25d699f2ebf489509cf05d3280ab31a2c4
Joaquim Stähli <joaquim.staehli@gmail.com>
2024-08-27 15:11:53 +0200

    Remove unused property

--- pillarbox-player/src/main/java/ch/srgssr/pillarbox/player/asset/Asset.kt
+++ pillarbox-player/src/main/java/ch/srgssr/pillarbox/player/asset/Asset.kt
@@ -4,7 +4,6 @@
  */
 package ch.srgssr.pillarbox.player.asset
 
-import androidx.media3.common.MediaItem
 import androidx.media3.common.MediaMetadata
 import androidx.media3.exoplayer.source.MediaSource
 import ch.srgssr.pillarbox.player.asset.timeRange.BlockedTimeRange
@@ -16,13 +15,11 @@ import ch.srgssr.pillarbox.player.tracker.MediaItemTrackerData
  * @property mediaSource The [MediaSource] used by the player to play something.
  * @property trackersData The [MediaItemTrackerData] to set to the [PillarboxData].
  * @property mediaMetadata The [MediaMetadata] to set to the player media item.
- * @property customData The custom data will be available as `tag` in [MediaItem.localConfiguration]
  * @property blockedTimeRanges The [BlockedTimeRange] list.
  */
 data class Asset(
     val mediaSource: MediaSource,
     val trackersData: MediaItemTrackerData = MediaItemTrackerData.EMPTY,
     val mediaMetadata: MediaMetadata = MediaMetadata.EMPTY,
-    val customData: Any? = null,
     val blockedTimeRanges: List<BlockedTimeRange> = emptyList(),
 )
