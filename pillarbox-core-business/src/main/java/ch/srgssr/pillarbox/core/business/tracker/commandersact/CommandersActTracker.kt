b73a51ebe7872ca2fdaf34daf711990350d1a03c
Joaquim Stähli <joaquim.staehli@gmail.com>
2024-09-24 17:07:49 +0200

    Fix CommandersAct lifecycle

--- pillarbox-core-business/src/main/java/ch/srgssr/pillarbox/core/business/tracker/commandersact/CommandersActTracker.kt
+++ pillarbox-core-business/src/main/java/ch/srgssr/pillarbox/core/business/tracker/commandersact/CommandersActTracker.kt
@@ -4,14 +4,10 @@
  */
 package ch.srgssr.pillarbox.core.business.tracker.commandersact
 
-import androidx.media3.common.Player
-import androidx.media3.common.Player.PositionInfo
 import androidx.media3.exoplayer.ExoPlayer
-import androidx.media3.exoplayer.analytics.AnalyticsListener
 import ch.srgssr.pillarbox.analytics.commandersact.CommandersAct
 import ch.srgssr.pillarbox.player.tracker.MediaItemTracker
 import kotlin.coroutines.CoroutineContext
-import kotlin.time.Duration.Companion.milliseconds
 
 /**
  * Commanders act tracker
@@ -24,23 +20,7 @@ import kotlin.time.Duration.Companion.milliseconds
 class CommandersActTracker(
     private val commandersAct: CommandersAct,
     private val coroutineContext: CoroutineContext,
-) : MediaItemTracker<CommandersActTracker.Data>, AnalyticsListener {
-
-    /**
-     * Stop reason
-     */
-    enum class StopReason {
-
-        /**
-         * When the player has been stopped, released or its current media item changes.
-         */
-        Stop,
-
-        /**
-         * When the player reaches the end of the media.
-         */
-        EoF
-    }
+) : MediaItemTracker<CommandersActTracker.Data> {
 
     /**
      * Data for CommandersAct
@@ -49,36 +29,26 @@ class CommandersActTracker(
      * @property sourceId TBD
      */
     data class Data(val assets: Map<String, String>, val sourceId: String? = null)
+
     private var analyticsStreaming: CommandersActStreaming? = null
-    private lateinit var currentData: Data
     private lateinit var player: ExoPlayer
 
     override fun start(player: ExoPlayer, data: Data) {
-        require(analyticsStreaming == null) { "AnalyticsStreaming already start" }
         this.player = player
-        commandersAct.enableRunningInBackground()
-        currentData = data
         analyticsStreaming = CommandersActStreaming(
-            commandersAct = commandersAct,
             player = player,
+            commandersAct = commandersAct,
             currentData = data,
-            coroutineContext = coroutineContext,
-        )
-        player.addAnalyticsListener(this)
-        analyticsStreaming?.let {
+            coroutineContext = coroutineContext
+        ).also {
             player.addAnalyticsListener(it)
         }
     }
 
     override fun stop(player: ExoPlayer) {
-        player.removeAnalyticsListener(this)
-        stop(StopReason.Stop, player.currentPosition)
-    }
-
-    private fun stop(reason: StopReason, positionMs: Long) {
         analyticsStreaming?.let {
             player.removeAnalyticsListener(it)
-            it.notifyStop(position = positionMs.milliseconds, reason == StopReason.EoF)
+            it.stop()
         }
         analyticsStreaming = null
     }
@@ -92,48 +62,6 @@ class CommandersActTracker(
     ) : MediaItemTracker.Factory<Data> {
         override fun create(): CommandersActTracker {
             return CommandersActTracker(commandersAct, coroutineContext)
-        }
-    }
-
-    override fun onPlaybackStateChanged(
-        eventTime: AnalyticsListener.EventTime,
-        @Player.State playbackState: Int,
-    ) {
-        when (playbackState) {
-            Player.STATE_ENDED -> stop(StopReason.EoF, player.currentPosition)
-            Player.STATE_IDLE -> stop(StopReason.Stop, player.currentPosition)
-            Player.STATE_READY -> {
-                if (analyticsStreaming == null) {
-                    start(player, currentData)
-                }
-            }
-
-            else -> Unit
-        }
-    }
-
-    /*
-     * On position discontinuity handle stop session if required
-     */
-    override fun onPositionDiscontinuity(
-        eventTime: AnalyticsListener.EventTime,
-        oldPosition: PositionInfo,
-        newPosition: PositionInfo,
-        @Player.DiscontinuityReason reason: Int,
-    ) {
-        val oldPositionMs = oldPosition.positionMs
-        when (reason) {
-            Player.DISCONTINUITY_REASON_REMOVE -> stop(StopReason.Stop, oldPositionMs)
-            Player.DISCONTINUITY_REASON_AUTO_TRANSITION -> {
-                stop(StopReason.EoF, oldPositionMs)
-                start(player, currentData)
-            }
-
-            else -> {
-                if (oldPosition.mediaItemIndex != newPosition.mediaItemIndex) {
-                    stop(StopReason.Stop, oldPositionMs)
-                }
-            }
         }
     }
 }
