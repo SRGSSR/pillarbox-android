5ba9b7c08560ae4dddb167fc82b423a18820bafa
Gaëtan Muller <m.gaetan89@gmail.com>
2024-10-01 16:36:43 +0200

    #550 Add SAM endpoints to `Lists`

--- pillarbox-demo-shared/src/main/java/ch/srgssr/pillarbox/demo/shared/di/PlayerModule.kt
+++ pillarbox-demo-shared/src/main/java/ch/srgssr/pillarbox/demo/shared/di/PlayerModule.kt
@@ -16,12 +16,31 @@ import ch.srgssr.pillarbox.demo.shared.source.CustomAssetLoader
 import ch.srgssr.pillarbox.demo.shared.ui.integrationLayer.data.ILRepository
 import ch.srgssr.pillarbox.player.PillarboxExoPlayer
 import ch.srgssr.pillarbox.player.source.PillarboxMediaSourceFactory
+import okhttp3.Interceptor
+import okhttp3.Response
 import java.net.URL
 
 /**
  * Dependencies to make custom Dependency Injection
  */
 object PlayerModule {
+    /**
+     * Prod host url
+     */
+    val IlHost.SAM_PROD: URL
+        get() = URL("https://il.srgssr.ch/sam/")
+
+    /**
+     * Test host url
+     */
+    val IlHost.SAM_TEST: URL
+        get() = URL("https://il-test.srgssr.ch/sam/")
+
+    /**
+     * Stage host url
+     */
+    val IlHost.SAM_STAGE: URL
+        get() = URL("https://il-stage.srgssr.ch/sam/")
 
     /**
      * Provide default player that allow to play urls and urns content from the SRG
@@ -43,15 +62,49 @@ object PlayerModule {
      */
     fun createIlRepository(context: Context, ilHost: URL = IlHost.DEFAULT): ILRepository {
         val okHttp = OkHttpModule.createOkHttpClient(context)
+            .newBuilder()
+            .addInterceptor(SamInterceptor(ilHost))
+            .build()
         val ilService = IlServiceModule.createIlService(okHttp, ilHost = providerIlHostFromUrl(ilHost))
         return ILRepository(dataProviderPaging = DataProviderPaging(ilService), ilService = ilService)
     }
 
     private fun providerIlHostFromUrl(ilHost: URL): ch.srg.dataProvider.integrationlayer.request.IlHost {
         return when (ilHost) {
+            IlHost.PROD -> ch.srg.dataProvider.integrationlayer.request.IlHost.PROD
             IlHost.STAGE -> ch.srg.dataProvider.integrationlayer.request.IlHost.STAGE
             IlHost.TEST -> ch.srg.dataProvider.integrationlayer.request.IlHost.TEST
+            IlHost.SAM_PROD -> ch.srg.dataProvider.integrationlayer.request.IlHost.PROD_SAM
+            IlHost.SAM_STAGE -> ch.srg.dataProvider.integrationlayer.request.IlHost.STAGE_SAM
+            IlHost.SAM_TEST -> ch.srg.dataProvider.integrationlayer.request.IlHost.TEST_SAM
             else -> ch.srg.dataProvider.integrationlayer.request.IlHost.PROD
+        }
+    }
+
+    private class SamInterceptor(ilHost: URL) : Interceptor {
+        private val ilHost: String = ilHost.toString()
+
+        override fun intercept(chain: Interceptor.Chain): Response {
+            val request = chain.request()
+            if (!isSamHost()) {
+                return chain.proceed(request)
+            }
+
+            val newUrl = request.url
+                .newBuilder()
+                .addQueryParameter("forceSAM", "true")
+                .build()
+            val newRequest = request.newBuilder()
+                .url(newUrl)
+                .build()
+
+            return chain.proceed(newRequest)
+        }
+
+        private fun isSamHost(): Boolean {
+            return ilHost == IlHost.SAM_PROD.toString() ||
+                ilHost == IlHost.SAM_STAGE.toString() ||
+                ilHost == IlHost.SAM_TEST.toString()
         }
     }
 }
