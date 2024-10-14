0e21bff590ed3f5eef4db0514d4da3eb9bb6f6b5
Joaquim Stähli <joaquim.staehli@gmail.com>
2024-10-14 09:03:37 +0200

    Fix issue with sam sending empty drm list

--- pillarbox-core-business/src/main/java/ch/srgssr/pillarbox/core/business/integrationlayer/ResourceSelector.kt
+++ pillarbox-core-business/src/main/java/ch/srgssr/pillarbox/core/business/integrationlayer/ResourceSelector.kt
@@ -13,20 +13,16 @@ import ch.srgssr.pillarbox.core.business.integrationlayer.data.Resource
  */
 internal class ResourceSelector {
     /**
-     * Select the first resource from chapter that is playable by the Player.
+     * Select the first resource from [chapter] that is playable by the Player.
      *
-     * @param chapter
+     * @param chapter The [Chapter].
      * @return null if no compatible resource is found.
      */
     @Suppress("SwallowedException")
     fun selectResourceFromChapter(chapter: Chapter): Resource? {
-        return try {
-            chapter.listResource?.first {
-                (it.type == Resource.Type.DASH || it.type == Resource.Type.HLS || it.type == Resource.Type.PROGRESSIVE) &&
-                    (it.drmList == null || it.drmList.any { drm -> drm.type == Drm.Type.WIDEVINE })
-            }
-        } catch (e: NoSuchElementException) {
-            null
+        return chapter.listResource?.find {
+            (it.type == Resource.Type.DASH || it.type == Resource.Type.HLS || it.type == Resource.Type.PROGRESSIVE) &&
+                (it.drmList.isNullOrEmpty() || it.drmList.any { drm -> drm.type == Drm.Type.WIDEVINE })
         }
     }
 }
