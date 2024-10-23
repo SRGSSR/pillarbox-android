b3ae71c25c6f9493eef4a9660f3520fed7755617
Gaëtan Muller <m.gaetan89@gmail.com>
2024-10-23 11:17:23 +0200

    Remove deprecated `DefaultHttpClient`

--- pillarbox-demo/build.gradle.kts
+++ pillarbox-demo/build.gradle.kts
@@ -89,6 +89,7 @@ dependencies {
     implementation(libs.guava)
     implementation(libs.kotlin.stdlib)
     implementation(libs.kotlinx.coroutines.core)
+    implementation(libs.kotlinx.datetime)
     implementation(libs.kotlinx.serialization.core)
     implementation(libs.okhttp)
     implementation(libs.srg.data)
