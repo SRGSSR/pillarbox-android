57bf81232b81abae8c1de5fd59fb3fc00f885246
Gaëtan Muller <gaetan.muller@rts.ch>
2024-11-25 11:39:25 +0100

    Upgrade to Coil 3.0.3 (#801)

--- pillarbox-demo-tv/src/main/java/ch/srgssr/pillarbox/demo/tv/DemoApplication.kt
+++ pillarbox-demo-tv/src/main/java/ch/srgssr/pillarbox/demo/tv/DemoApplication.kt
@@ -2,13 +2,9 @@
  * Copyright (c) SRG SSR. All rights reserved.
  * License information is available from the LICENSE file.
  */
-package ch.srgssr.pillarbox.demo
+package ch.srgssr.pillarbox.demo.tv
 
 import android.app.Application
-import ch.srgssr.pillarbox.analytics.AnalyticsConfig
-import ch.srgssr.pillarbox.analytics.SRGAnalytics.initSRGAnalytics
-import ch.srgssr.pillarbox.analytics.UserConsent
-import ch.srgssr.pillarbox.analytics.comscore.ComScoreUserConsent
 import ch.srgssr.pillarbox.player.network.PillarboxOkHttp
 import coil3.ImageLoader
 import coil3.PlatformContext
@@ -18,27 +14,9 @@ import coil3.network.cachecontrol.CacheControlCacheStrategy
 import coil3.network.okhttp.OkHttpNetworkFetcherFactory
 
 /**
- * Demo application that initializes SRG Analytics and Coil.
+ * Demo application that sets up Coil.
  */
 class DemoApplication : Application(), SingletonImageLoader.Factory {
-
-    override fun onCreate() {
-        super.onCreate()
-        // Defaults values
-        val initialUserConsent = UserConsent(
-            comScore = ComScoreUserConsent.UNKNOWN,
-            commandersActConsentServices = emptyList()
-        )
-        val config = AnalyticsConfig(
-            vendor = AnalyticsConfig.Vendor.SRG,
-            nonLocalizedApplicationName = "Pillarbox",
-            appSiteName = "pillarbox-demo-android",
-            sourceKey = AnalyticsConfig.SOURCE_KEY_SRG_DEBUG,
-            userConsent = initialUserConsent
-        )
-        initSRGAnalytics(config = config)
-    }
-
     @OptIn(ExperimentalCoilApi::class)
     override fun newImageLoader(context: PlatformContext): ImageLoader {
         return ImageLoader.Builder(context)
