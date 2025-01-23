46a29faac49bd28650c0429a6b10c5f877332629
Gaëtan Muller <m.gaetan89@gmail.com>
2025-01-23 14:19:28 +0100

    Add a `PillarboxCastPlayer` and update the demo

--- pillarbox-demo-cast/src/main/java/ch/srgssr/pillarbox/demo/cast/MainActivity.kt
+++ pillarbox-demo-cast/src/main/java/ch/srgssr/pillarbox/demo/cast/MainActivity.kt
@@ -7,18 +7,22 @@ package ch.srgssr.pillarbox.demo.cast
 import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.fragment.app.FragmentActivity
+import androidx.media3.cast.SessionAvailabilityListener
+import androidx.media3.common.MediaItem
+import androidx.media3.common.MimeTypes
+import ch.srgssr.pillarbox.cast.PillarboxCastPlayer
 import ch.srgssr.pillarbox.cast.getCastContext
 import ch.srgssr.pillarbox.cast.widget.CastButton
 import ch.srgssr.pillarbox.demo.cast.ui.theme.PillarboxTheme
+import ch.srgssr.pillarbox.ui.exoplayer.ExoPlayerView
 
 /**
  * Activity showing how to use Cast with Pillarbox.
@@ -26,23 +30,42 @@ import ch.srgssr.pillarbox.demo.cast.ui.theme.PillarboxTheme
 class MainActivity : FragmentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        // Force creation CastContext
-        getCastContext()
 
         enableEdgeToEdge()
+
         setContent {
+            val player = remember {
+                PillarboxCastPlayer(getCastContext()).apply {
+                    val mediaItem = MediaItem.Builder()
+                        .setMimeType(MimeTypes.VIDEO_MP4)
+                        .setUri("https://storage.googleapis.com/wvmedia/clear/h264/tears/tears.mpd")
+                        .build()
+
+                    setSessionAvailabilityListener(object : SessionAvailabilityListener {
+                        override fun onCastSessionAvailable() {
+                            setMediaItem(mediaItem)
+                        }
+
+                        override fun onCastSessionUnavailable() {
+                            release()
+                        }
+                    })
+                }
+            }
+
             PillarboxTheme {
                 Scaffold(
                     modifier = Modifier.fillMaxSize(),
                     floatingActionButton = {
                         CastButton()
-                    }
+                    },
                 ) { innerPadding ->
-                    Box(
-                        modifier = Modifier.padding(innerPadding)
-                    ) {
-                        Text(text = "Demo showing the cast button!")
-                    }
+                    ExoPlayerView(
+                        player = player,
+                        modifier = Modifier
+                            .padding(innerPadding)
+                            .fillMaxSize(),
+                    )
                 }
             }
         }
