ad3ef5a2460db5402782117f8a928d7056c92fca
Joaquim Stähli <joaquim.staehli@gmail.com>
2025-01-30 10:42:01 +0100

    Tv Cast receiver from the doc

---
+++ pillarbox-demo-tv/src/main/java/ch/srgssr/pillarbox/demo/tv/cast/DemoReceiverOptionsProvider.kt
@@ -0,0 +1,17 @@
+/*
+ * Copyright (c) SRG SSR. All rights reserved.
+ * License information is available from the LICENSE file.
+ */
+package ch.srgssr.pillarbox.demo.tv.cast
+
+import android.content.Context
+import com.google.android.gms.cast.tv.CastReceiverOptions
+import com.google.android.gms.cast.tv.ReceiverOptionsProvider
+
+class DemoReceiverOptionsProvider : ReceiverOptionsProvider {
+    override fun getOptions(context: Context): CastReceiverOptions {
+        return CastReceiverOptions.Builder(context)
+            .setCastAppId("16509351") // Joaquim Stähli cast application
+            .build()
+    }
+}
