2bcd87fa7a21d919031a1141c72459631808100c
Joaquim Stähli <joaquim.staehli@gmail.com>
2025-02-28 10:36:00 +0100

    Replace navigation_bu_distributer with content_bu_owner

--- pillarbox-analytics/src/main/java/ch/srgssr/pillarbox/analytics/commandersact/CommandersActLabels.kt
+++ pillarbox-analytics/src/main/java/ch/srgssr/pillarbox/analytics/commandersact/CommandersActLabels.kt
@@ -23,7 +23,7 @@ enum class CommandersActLabels(val label: String) {
 
     // Page View
     NAVIGATION_LEVEL_I("navigation_level_"),
-    NAVIGATION_BU_DISTRIBUTER("navigation_bu_distributer"),
+    NAVIGATION_CONTENT_BU_OWNER("content_bu_owner"),
 
     // User consent
     CONSENT_SERVICES("consent_services")
