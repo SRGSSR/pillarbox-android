342df3509a04f887dc72bc7097b07a00269de4d8
Joaquim Stähli <joaquim.staehli@gmail.com>
2025-03-28 09:28:22 +0100

    Implementation with flow

--- pillarbox-demo-cast/src/main/java/ch/srgssr/pillarbox/demo/cast/MainViewModel.kt
+++ pillarbox-demo-cast/src/main/java/ch/srgssr/pillarbox/demo/cast/MainViewModel.kt
@@ -6,15 +6,20 @@ package ch.srgssr.pillarbox.demo.cast
 
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
-import androidx.media3.cast.SessionAvailabilityListener
+import androidx.lifecycle.viewModelScope
 import androidx.media3.common.Player
 import ch.srgssr.pillarbox.cast.PillarboxCastPlayer
 import ch.srgssr.pillarbox.cast.getCastContext
+import ch.srgssr.pillarbox.cast.isCastSessionAvailableAsFlow
 import ch.srgssr.pillarbox.cast.isConnected
 import ch.srgssr.pillarbox.core.business.PillarboxExoPlayer
 import ch.srgssr.pillarbox.core.business.cast.PillarboxCastPlayer
 import ch.srgssr.pillarbox.player.extension.getCurrentMediaItems
-import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.distinctUntilChanged
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.flow.stateIn
 
 /**
  * ViewModel that olds current player and handle local to remote player switch.
@@ -30,19 +35,16 @@ class MainViewModel(application: Application) : AndroidViewModel(application) {
     /**
      * The current player, it can be either a [PillarboxCastPlayer] or a [PillarboxExoPlayer].
      */
-    val currentPlayer = MutableStateFlow(if (application.getCastContext().isConnected()) castPlayer else localPlayer)
-
-    init {
-        castPlayer.setSessionAvailabilityListener(object : SessionAvailabilityListener {
-            override fun onCastSessionAvailable() {
-                switchPlayer(castPlayer)
-            }
-
-            override fun onCastSessionUnavailable() {
-                switchPlayer(localPlayer)
-            }
-        })
+    val currentPlayer = castPlayer.isCastSessionAvailableAsFlow().map {
+        if (it) castPlayer else localPlayer
     }
+        .distinctUntilChanged()
+        .onEach { switchPlayer(it) }
+        .stateIn(
+            viewModelScope,
+            SharingStarted.WhileSubscribed(5_000),
+            if (application.getCastContext().isConnected()) castPlayer else localPlayer
+        )
 
     override fun onCleared() {
         castPlayer.release()
@@ -50,12 +52,11 @@ class MainViewModel(application: Application) : AndroidViewModel(application) {
     }
 
     private fun switchPlayer(player: Player) {
-        val oldPlayer = currentPlayer.value
+        val oldPlayer = if (player is PillarboxCastPlayer) localPlayer else castPlayer
         if (oldPlayer == player) return
         player.repeatMode = oldPlayer.repeatMode
         player.playWhenReady = oldPlayer.playWhenReady
         player.setMediaItems(oldPlayer.getCurrentMediaItems(), oldPlayer.currentMediaItemIndex, oldPlayer.currentPosition)
-        currentPlayer.value = player
 
         oldPlayer.stop()
         oldPlayer.clearMediaItems()
