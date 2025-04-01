90801f1509f034d9f2a1e0200801ce8362d7cfa9
Gaëtan Muller <m.gaetan89@gmail.com>
2025-04-01 08:55:05 +0200

    Remove duplicated dependencies

--- pillarbox-demo-cast/build.gradle.kts
+++ pillarbox-demo-cast/build.gradle.kts
@@ -21,8 +21,6 @@ dependencies {
     implementation(libs.androidx.compose.material.icons.core)
     implementation(libs.androidx.compose.material.icons.extended)
     implementation(libs.androidx.compose.material3)
-    implementation(libs.androidx.compose.material.icons.core)
-    implementation(libs.androidx.compose.material.icons.extended)
     implementation(libs.androidx.compose.runtime)
     implementation(libs.androidx.compose.ui)
     implementation(libs.androidx.compose.ui.graphics)
