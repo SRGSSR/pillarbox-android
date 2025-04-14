d97f96c937f57de3cabd8f3bc5d7eb7b2cdeb2d8
Joaquim Stähli <joaquim.staehli@gmail.com>
2025-04-14 10:50:06 +0200

    Update doc

--- pillarbox-cast/src/main/java/ch/srgssr/pillarbox/cast/CastTrackSelector.kt
+++ pillarbox-cast/src/main/java/ch/srgssr/pillarbox/cast/CastTrackSelector.kt
@@ -14,14 +14,14 @@ import com.google.android.gms.cast.MediaTrack
 interface CastTrackSelector {
 
     /**
-     * Returns the indices of the currently selected media tracks.
+     * Returns the track ids of the currently selected media tracks.
      *
      * This function determines the active tracks based on the provided [parameters]
-     * and the available [tracks]. It returns an array containing the indices of these tracks.
+     * and the available [tracks]. It returns an array containing the ids of these tracks.
      *
      * @param parameters The track selection preferences.
      * @param tracks The available media tracks.
-     * @return An array of track indices for the selected tracks. Returns an empty array if no tracks are selected.
+     * @return An array of track ids for the selected tracks. Returns an empty array if no tracks are selected.
      * @see TrackSelectionParameters
      * @see Tracks
      */
