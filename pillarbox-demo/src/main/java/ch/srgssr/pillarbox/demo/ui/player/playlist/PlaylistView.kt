37b026e0fb654b0040bb7ba10dc1faf5595be616
Joaquim Stähli <joaquim.staehli@gmail.com>
2025-06-05 15:48:38 +0200

    Fallback on index

--- pillarbox-demo/src/main/java/ch/srgssr/pillarbox/demo/ui/player/playlist/PlaylistView.kt
+++ pillarbox-demo/src/main/java/ch/srgssr/pillarbox/demo/ui/player/playlist/PlaylistView.kt
@@ -129,7 +129,7 @@ fun PlaylistView(
                 },
                 key = { index ->
                     if (player.currentTimeline.isEmpty) {
-                        Any()
+                        index
                     } else {
                         player.currentTimeline.getWindow(index, Timeline.Window()).uid.hashCode()
                     }
