863e18720861291d3764d26dda8f3e012b0ef642
Joaquim Stähli <joaquim.staehli@gmail.com>
2025-06-18 16:24:59 +0200

    Redo communication between MediaSession and Controller

--- pillarbox-player/src/main/java/ch/srgssr/pillarbox/player/session/PillarboxSessionCommands.kt
+++ pillarbox-player/src/main/java/ch/srgssr/pillarbox/player/session/PillarboxSessionCommands.kt
@@ -8,39 +8,34 @@ import android.os.Bundle
 import androidx.media3.session.SessionCommand
 
 internal object PillarboxSessionCommands {
-    const val SMOOTH_SEEKING_ARG = "pillarbox.smoothSeekingEnabled"
-    const val TRACKER_ENABLED_ARG = "pillarbox.trackerEnabled"
+    const val ARG_SMOOTH_SEEKING = "pillarbox.smoothSeekingEnabled"
+    const val ARG_TRACKER_ENABLED = "pillarbox.trackerEnabled"
     const val ARG_CHAPTER_CHANGED = "pillarbox.range.chapter"
     const val ARG_BLOCKED = "pillarbox.range.blocked"
     const val ARG_CREDIT = "pillarbox.range.credit"
+
+    const val ARG_PLAYBACK_METRICS = "pillarbox.playback.metrics"
 
     const val CHAPTER_CHANGED = "pillarbox.chapter.changed"
     const val BLOCKED_CHANGED = "pillarbox.blocked.changed"
     const val CREDIT_CHANGED = "pillarbox.credit.changed"
     const val SMOOTH_SEEKING_ENABLED = "pillarbox.smooth.seeking.enabled"
     const val TRACKER_ENABLED = "pillarbox.tracker.enabled"
-
-    /**
-     * Place holder command
-     */
-    val COMMAND_SMOOTH_SEEKING_ENABLED = SessionCommand(SMOOTH_SEEKING_ENABLED, Bundle.EMPTY)
-
-    /**
-     * Place holder command
-     */
-    val COMMAND_TRACKER_ENABLED = SessionCommand(TRACKER_ENABLED, Bundle.EMPTY)
+    const val CURRENT_PLAYBACK_METRICS = "pillarbox.current.playback.metrics"
 
     val COMMAND_CHAPTER_CHANGED = SessionCommand(CHAPTER_CHANGED, Bundle.EMPTY)
-
     val COMMAND_BLOCKED_CHANGED = SessionCommand(BLOCKED_CHANGED, Bundle.EMPTY)
-
     val COMMAND_CREDIT_CHANGED = SessionCommand(CREDIT_CHANGED, Bundle.EMPTY)
+    val COMMAND_GET_CURRENT_PLAYBACK_METRICS = SessionCommand(CURRENT_PLAYBACK_METRICS, Bundle.EMPTY)
+    val COMMAND_SMOOTH_SEEKING_ENABLED = SessionCommand(SMOOTH_SEEKING_ENABLED, Bundle.EMPTY)
+    val COMMAND_TRACKER_ENABLED = SessionCommand(TRACKER_ENABLED, Bundle.EMPTY)
 
-    fun setSmoothSeekingEnabled(smoothSeekingEnabled: Boolean): SessionCommand {
-        return SessionCommand(SMOOTH_SEEKING_ENABLED, Bundle().apply { putBoolean(SMOOTH_SEEKING_ARG, smoothSeekingEnabled) })
-    }
-
-    fun setTrackerEnabled(trackerEnabled: Boolean): SessionCommand {
-        return SessionCommand(TRACKER_ENABLED, Bundle().apply { putBoolean(TRACKER_ENABLED_ARG, trackerEnabled) })
-    }
+    val AVAILABLE_COMMANDS = listOf(
+        COMMAND_SMOOTH_SEEKING_ENABLED,
+        COMMAND_TRACKER_ENABLED,
+        COMMAND_CHAPTER_CHANGED,
+        COMMAND_CREDIT_CHANGED,
+        COMMAND_BLOCKED_CHANGED,
+        COMMAND_GET_CURRENT_PLAYBACK_METRICS
+    )
 }
