f5e754e2f76b31a3a31996b70e716e1b5b9fc8a3
Joaquim Stähli <joaquim.staehli@gmail.com>
2025-06-26 12:54:18 +0200

    Demonstrate thumbnail feature with MediaController
    The showcase add CastPlayer to teste with player that doesn't support it.

--- pillarbox-demo/src/main/java/ch/srgssr/pillarbox/demo/ui/showcases/layouts/thumbnail/ThumbnailViewModel.kt
+++ pillarbox-demo/src/main/java/ch/srgssr/pillarbox/demo/ui/showcases/layouts/thumbnail/ThumbnailViewModel.kt
@@ -6,18 +6,26 @@ package ch.srgssr.pillarbox.demo.ui.showcases.layouts.thumbnail
 
 import android.app.Application
 import android.graphics.Bitmap
+import android.util.Log
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.viewModelScope
+import androidx.media3.cast.SessionAvailabilityListener
+import androidx.media3.common.Timeline
 import androidx.media3.exoplayer.image.ImageOutput
+import androidx.media3.session.SessionToken
+import ch.srgssr.pillarbox.cast.PillarboxCastPlayer
 import ch.srgssr.pillarbox.core.business.PillarboxExoPlayer
 import ch.srgssr.pillarbox.core.business.SRGMediaItem
+import ch.srgssr.pillarbox.core.business.cast.PillarboxCastPlayer
 import ch.srgssr.pillarbox.demo.shared.data.samples.SamplesSRG
 import ch.srgssr.pillarbox.demo.shared.data.samples.SamplesUnifiedStreaming
-import ch.srgssr.pillarbox.ui.ProgressTrackerState
-import ch.srgssr.pillarbox.ui.SmoothProgressTrackerState
+import ch.srgssr.pillarbox.player.PillarboxPlayer
+import ch.srgssr.pillarbox.player.extension.getCurrentMediaItems
+import ch.srgssr.pillarbox.player.session.PillarboxMediaController
+import ch.srgssr.pillarbox.player.session.PillarboxMediaSession
 import coil3.imageLoader
 import coil3.request.ErrorResult
 import coil3.request.ImageRequest
@@ -25,19 +33,20 @@ import coil3.request.SuccessResult
 import coil3.request.allowConversionToBitmap
 import coil3.size.Scale
 import coil3.toBitmap
+import kotlinx.coroutines.guava.await
+import kotlinx.coroutines.launch
+import kotlin.time.Duration.Companion.milliseconds
 
 /**
- * A ViewModel to demonstrate how to work with Image track.
+ * A ViewModel to demonstrate how to work with Image track regardless the player. Of course, when casting, some features aren't supported.
  *
  * @param application The [Application].
  */
-class ThumbnailViewModel(application: Application) : AndroidViewModel(application), ImageOutput {
+class ThumbnailViewModel(application: Application) : AndroidViewModel(application), ImageOutput, SessionAvailabilityListener {
     private val imageLoader = application.imageLoader
+    private val mediaSession: PillarboxMediaSession
 
-    /**
-     * Player
-     */
-    val player = PillarboxExoPlayer(application) {
+    private val localPlayer = PillarboxExoPlayer(application) {
         srgAssetLoader(application) {
             spriteSheetLoader { spriteSheet ->
                 val request = ImageRequest.Builder(application)
@@ -53,6 +62,7 @@ class ThumbnailViewModel(application: Application) : AndroidViewModel(applicatio
             }
         }
     }
+    private val castPlayer: PillarboxCastPlayer = PillarboxCastPlayer(application).apply { setSessionAvailabilityListener(this@ThumbnailViewModel) }
 
     /**
      * Thumbnail
@@ -61,21 +71,61 @@ class ThumbnailViewModel(application: Application) : AndroidViewModel(applicatio
         private set
 
     /**
-     * Progress tracker state
+     * Player may be null until [PillarboxMediaController] is built.
      */
-    val progressTrackerState: ProgressTrackerState = SmoothProgressTrackerState(player, viewModelScope, this)
+    var player by mutableStateOf<PillarboxPlayer?>(null)
+        private set
 
     init {
-        player.prepare()
-        player.addMediaItem(SRGMediaItem("urn:srf:video:881be9c2-65ec-4fa9-ba4a-926d15d046ef"))
-        player.addMediaItem(SamplesSRG.OnDemandHorizontalVideo.toMediaItem())
-        player.addMediaItem(SRGMediaItem("urn:rsi:video:2366175"))
-        player.addMediaItem(SamplesUnifiedStreaming.DASH_Tiled_Thumbnails.toMediaItem())
-        player.addMediaItem(SamplesUnifiedStreaming.DASH_TrickPlay.toMediaItem())
+        mediaSession = PillarboxMediaSession.Builder(application, localPlayer)
+            .setId("ThumbnailMediaSession")
+            .build()
+
+        viewModelScope.launch {
+            val sessionToken = SessionToken.createSessionToken(application, mediaSession.token).await()
+            // Connect PillarboxMediaController to the MediaSession instead of the MediaSessionService. We don't need background playback.
+            player = PillarboxMediaController.Builder(application, sessionToken).build().apply {
+                prepare()
+                addMediaItem(SamplesSRG.OnDemandHorizontalVideo.toMediaItem())
+                addMediaItem(SRGMediaItem("urn:srf:video:881be9c2-65ec-4fa9-ba4a-926d15d046ef"))
+                addMediaItem(SRGMediaItem("urn:rsi:video:2366175"))
+                addMediaItem(SamplesUnifiedStreaming.DASH_Tiled_Thumbnails.toMediaItem())
+                addMediaItem(SamplesUnifiedStreaming.DASH_TrickPlay.toMediaItem())
+                addListener(object : PillarboxPlayer.Listener {
+                    override fun onTimelineChanged(timeline: Timeline, reason: Int) {
+                        Log.d("THUMBNAIL", "player.duration = ${player?.duration?.milliseconds}")
+                    }
+                })
+            }
+        }
+        castPlayer.addListener(object : PillarboxPlayer.Listener {
+            override fun onTimelineChanged(timeline: Timeline, reason: Int) {
+                Log.d("THUMBNAIL", "castPlayer.duration = ${castPlayer.duration.milliseconds}")
+            }
+        })
+    }
+
+    override fun onCastSessionAvailable() {
+        localPlayer.stop()
+        castPlayer.setMediaItems(localPlayer.getCurrentMediaItems(), localPlayer.currentMediaItemIndex, localPlayer.currentPosition)
+        castPlayer.prepare()
+        castPlayer.play()
+        mediaSession.player = castPlayer
+    }
+
+    override fun onCastSessionUnavailable() {
+        localPlayer.seekTo(castPlayer.currentMediaItemIndex, castPlayer.currentPosition)
+        castPlayer.stop()
+        localPlayer.prepare()
+        localPlayer.play()
+        mediaSession.player = localPlayer
     }
 
     override fun onCleared() {
-        player.release()
+        player?.release()
+        mediaSession.release()
+        localPlayer.release()
+        castPlayer.release()
     }
 
     override fun onImageAvailable(presentationTimeUs: Long, bitmap: Bitmap) {
