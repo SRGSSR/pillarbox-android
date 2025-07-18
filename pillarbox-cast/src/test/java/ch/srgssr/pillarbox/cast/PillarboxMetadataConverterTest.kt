17ed5e7769b38aba5db04ba86448bec4eb4047d7
Joaquim Stähli <joaquim.staehli@gmail.com>
2025-07-18 08:49:11 +0200

    Encode and decode BlockTimeRange too

--- pillarbox-cast/src/test/java/ch/srgssr/pillarbox/cast/PillarboxMetadataConverterTest.kt
+++ pillarbox-cast/src/test/java/ch/srgssr/pillarbox/cast/PillarboxMetadataConverterTest.kt
@@ -5,7 +5,9 @@
 package ch.srgssr.pillarbox.cast
 
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import ch.srgssr.pillarbox.player.asset.timeRange.BlockedTimeRange
 import ch.srgssr.pillarbox.player.asset.timeRange.Chapter
+import ch.srgssr.pillarbox.player.asset.timeRange.Credit
 import org.json.JSONObject
 import org.junit.runner.RunWith
 import kotlin.test.Test
@@ -15,7 +17,7 @@ import kotlin.test.assertEquals
 class PillarboxMetadataConverterTest {
 
     @Test
-    fun `fromJson from  toJson give the initial result`() {
+    fun `chapter fromJson from toJson give the initial result`() {
         val listChapter = listOf(
             Chapter(
                 id = "urn:0",
@@ -44,5 +46,51 @@ class PillarboxMetadataConverterTest {
         PillarboxMetadataConverter.appendChapters(customData, listChapter)
         val parsedChapters = PillarboxMetadataConverter.decodeChapters(customData)
         assertEquals(listChapter, parsedChapters)
+    }
+
+    @Test
+    fun `Credit fromJson from toJson give the initial result`() {
+        val listCredits = listOf(
+            Credit.Opening(
+                start = 1000,
+                end = 2000,
+            ),
+            Credit.Closing(
+                start = 25000,
+                end = 30000,
+            ),
+        )
+
+        val customData = JSONObject()
+        PillarboxMetadataConverter.appendCredits(customData, listCredits)
+        val parsedCredits = PillarboxMetadataConverter.decodeCredits(customData)
+        assertEquals(listCredits, parsedCredits)
+    }
+
+    @Test
+    fun `BlockedTimeRanges fromJson from toJson give the initial result`() {
+        val listBlockedTimeRanges = listOf(
+            BlockedTimeRange(
+                start = 0,
+                end = 5000,
+                reason = "ILLEGAL"
+            ),
+            BlockedTimeRange(
+                start = 10000,
+                end = 12000,
+                reason = null,
+            ),
+            BlockedTimeRange(
+                start = 10000,
+                end = 12000,
+                reason = null,
+                id = "id"
+            )
+        )
+
+        val customData = JSONObject()
+        PillarboxMetadataConverter.appendBlockedTimeRanges(customData, listBlockedTimeRanges)
+        val parsedBlockedTimeRanges = PillarboxMetadataConverter.decodeBlockedTimeRanges(customData)
+        assertEquals(listBlockedTimeRanges, parsedBlockedTimeRanges)
     }
 }
