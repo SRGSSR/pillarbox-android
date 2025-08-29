046597857e268dbb9bd0e05e3fe64b8757ca5ad1
Joaquim Stähli <joaquim.staehli@gmail.com>
2025-08-29 10:22:39 +0200

    Cast to local player synchronizer

--- pillarbox-demo/src/main/java/ch/srgssr/pillarbox/demo/ui/showcases/integrations/cast/CastShowcaseViewModel.kt
+++ pillarbox-demo/src/main/java/ch/srgssr/pillarbox/demo/ui/showcases/integrations/cast/CastShowcaseViewModel.kt
@@ -8,30 +8,26 @@ import android.app.Application
 import androidx.core.content.ContextCompat
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.viewModelScope
-import androidx.media3.common.Player
 import androidx.mediarouter.media.MediaControlIntent
 import androidx.mediarouter.media.MediaRouteSelector
 import ch.srgssr.media.maestro.MediaRouteButton
+import ch.srgssr.pillarbox.cast.CastPlayerSynchronizer
 import ch.srgssr.pillarbox.cast.PillarboxCastPlayer
 import ch.srgssr.pillarbox.cast.getCastContext
-import ch.srgssr.pillarbox.cast.isCastSessionAvailableAsFlow
-import ch.srgssr.pillarbox.cast.isConnected
 import ch.srgssr.pillarbox.core.business.PillarboxExoPlayer
 import ch.srgssr.pillarbox.core.business.cast.PillarboxCastPlayer
 import ch.srgssr.pillarbox.player.PillarboxExoPlayer
+import ch.srgssr.pillarbox.player.PillarboxPlayer
 import ch.srgssr.pillarbox.player.currentMediaMetadataAsFlow
-import ch.srgssr.pillarbox.player.extension.getCurrentMediaItems
 import coil3.SingletonImageLoader
 import coil3.asDrawable
 import coil3.request.ImageRequest
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.flow.SharingStarted.Companion.WhileSubscribed
-import kotlinx.coroutines.flow.distinctUntilChanged
 import kotlinx.coroutines.flow.flatMapLatest
 import kotlinx.coroutines.flow.flowOn
 import kotlinx.coroutines.flow.map
-import kotlinx.coroutines.flow.onEach
 import kotlinx.coroutines.flow.stateIn
 import androidx.mediarouter.R as mediaRouterR
 
@@ -46,16 +42,26 @@ class CastShowcaseViewModel(application: Application) : AndroidViewModel(applica
     private val castContext = application.getCastContext()
     private val castPlayer = PillarboxCastPlayer(application)
     private val localPlayer = PillarboxExoPlayer(application)
+    private val castPlayerSynchronizer = CastPlayerSynchronizer(
+        castContext = castContext,
+        coroutineScope = viewModelScope,
+        castPlayer = castPlayer,
+        localPlayer = localPlayer,
+        playerSynchronizer = object : CastPlayerSynchronizer.DefaultPlayerSynchronizer() {
+            override fun onPlayerChanged(oldPlayer: PillarboxPlayer, newPlayer: PillarboxPlayer) {
+                super.onPlayerChanged(oldPlayer, newPlayer)
+                newPlayer.prepare()
+                oldPlayer.stop()
+                oldPlayer.clearMediaItems()
+            }
+        }
+    )
     private val defaultArtwork by lazy { ContextCompat.getDrawable(application, mediaRouterR.drawable.ic_mr_button_disconnected_dark) }
 
     /**
      * The current player, it can be either a [PillarboxCastPlayer] or a [PillarboxExoPlayer].
      */
-    val currentPlayer = castPlayer.isCastSessionAvailableAsFlow()
-        .map { if (it) castPlayer else localPlayer }
-        .distinctUntilChanged()
-        .onEach { switchPlayer(it) }
-        .stateIn(viewModelScope, WhileSubscribed(5_000), if (castContext.isConnected()) castPlayer else localPlayer)
+    val currentPlayer = castPlayerSynchronizer.currentPlayer
 
     /**
      * The [MediaRouteSelector] to use on the [MediaRouteButton].
@@ -89,16 +95,5 @@ class CastShowcaseViewModel(application: Application) : AndroidViewModel(applica
     override fun onCleared() {
         castPlayer.release()
         localPlayer.release()
-    }
-
-    private fun switchPlayer(player: Player) {
-        val oldPlayer = if (player is PillarboxCastPlayer) localPlayer else castPlayer
-        if (oldPlayer == player || (oldPlayer == localPlayer && oldPlayer.playbackState == Player.STATE_IDLE)) return
-        player.repeatMode = oldPlayer.repeatMode
-        player.playWhenReady = oldPlayer.playWhenReady
-        player.setMediaItems(oldPlayer.getCurrentMediaItems(), oldPlayer.currentMediaItemIndex, oldPlayer.currentPosition)
-        player.prepare()
-        oldPlayer.stop()
-        oldPlayer.clearMediaItems()
     }
 }
