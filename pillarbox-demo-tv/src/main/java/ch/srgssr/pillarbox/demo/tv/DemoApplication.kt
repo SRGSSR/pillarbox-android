43e59641097e8ceb239ecc1f4f558c9bbd8b1e21
Joaquim Stähli <joaquim.staehli@gmail.com>
2026-04-21 09:02:08 +0200

    Initialize analytics for DemoTV too

--- pillarbox-demo-tv/src/main/java/ch/srgssr/pillarbox/demo/tv/DemoApplication.kt
+++ pillarbox-demo-tv/src/main/java/ch/srgssr/pillarbox/demo/tv/DemoApplication.kt
@@ -9,6 +9,11 @@ import android.widget.Toast
 import androidx.lifecycle.DefaultLifecycleObserver
 import androidx.lifecycle.LifecycleOwner
 import androidx.lifecycle.ProcessLifecycleOwner
+import ch.srgssr.pillarbox.analytics.AnalyticsConfig
+import ch.srgssr.pillarbox.analytics.SRGAnalytics.initSRGAnalytics
+import ch.srgssr.pillarbox.analytics.SourceKey
+import ch.srgssr.pillarbox.analytics.UserConsent
+import ch.srgssr.pillarbox.analytics.comscore.ComScoreUserConsent
 import ch.srgssr.pillarbox.player.network.PillarboxOkHttp
 import coil3.ImageLoader
 import coil3.PlatformContext
@@ -38,6 +43,20 @@ class DemoApplication : Application(), SingletonImageLoader.Factory {
             }
         })
         ProcessLifecycleOwner.get().lifecycle.addObserver(MyLifecycleObserver())
+
+        // Defaults values
+        val initialUserConsent = UserConsent(
+            comScore = ComScoreUserConsent.UNKNOWN,
+            commandersActConsentServices = emptyList()
+        )
+        val config = AnalyticsConfig(
+            vendor = AnalyticsConfig.Vendor.SRG,
+            nonLocalizedApplicationName = "Pillarbox",
+            appSiteName = "pillarbox-demo-android",
+            sourceKey = SourceKey.DEVELOPMENT,
+            userConsent = initialUserConsent
+        )
+        initSRGAnalytics(config = config)
     }
 
     @OptIn(ExperimentalCoilApi::class)
